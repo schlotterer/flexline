1df17bbe65c02dd3079e6b727490894dd0481091
schlotterer <joelschlotterer@gmail.com>
2024-01-16 07:22:03 -0600

    start roughing out template changes

---
+++ patterns/header-over-hero.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Title: Header transparent over hero.
+ * Slug: flexline/header-over-hero
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"},"margin":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:30px;padding-bottom:30px">
+<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide">
+<!-- wp:site-title /-->
+<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true}} /-->
+</div>
+<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
