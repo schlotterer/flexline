c4573ff0af05fcc1ffc126ed1340023135e4cd0c
schlotterer <joelschlotterer@gmail.com>
2024-01-17 14:16:15 -0600

    test first header

--- patterns/header-default.php
+++ patterns/header-default.php
@@ -6,12 +6,20 @@
  * Block Types: core/template-part/header
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"30px","bottom":"30px"},"margin":{"top":"0px"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:30px;padding-bottom:30px">
-<!-- wp:group {"align":"wide","layout":{"type":"flex","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide">
-<!-- wp:site-title /-->
-<!-- wp:navigation {"layout":{"type":"flex","setCascadingProperties":true}} /-->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"15px","bottom":"15px","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"0px"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0px;padding-top:15px;padding-right:var(--wp--preset--spacing--small);padding-bottom:15px;padding-left:var(--wp--preset--spacing--small)">
+<!-- wp:site-logo {"width":225} /-->
+
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"right","verticalAlignment":"center"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+    <!-- wp:navigation {"openSubmenusOnClick":true,"overlayMenu":"never","layout":{"type":"flex","setCascadingProperties":true,"justifyContent":"right"},"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group">
+    <!-- wp:navigation {"style":{"spacing":{"blockGap":"var:preset|spacing|small"},"typography":{"textTransform":"uppercase"}}} /--></div>
+<!-- /wp:group -->
 </div>
 <!-- /wp:group -->
 </div>
