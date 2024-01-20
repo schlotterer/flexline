263c881ffe3b6dba91eb24dab7f0346496576c5d
schlotterer <joelschlotterer@gmail.com>
2024-01-19 22:27:50 -0600

    tight up utitlity

--- patterns/utility.php
+++ patterns/utility.php
@@ -6,7 +6,7 @@
  * Block Types: core/template-part/utility
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"right":"0","left":"0","top":"0","bottom":"0"}},"dimensions":{"minHeight":"0px"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
+<!-- wp:group {"style":{"spacing":{"blockGap":"0","margin":{"top":"0","bottom":"0"},"padding":{"right":"0","left":"0","top":"0","bottom":"0"}},"dimensions":{"minHeight":"0px"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
 <div class="wp-block-group" style="min-height:0px;margin-top:0;margin-bottom:0;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:search {"label":"Search","showLabel":false,"width":null,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-only","buttonUseIcon":true,"isSearchFieldHidden":true,"style":{"layout":{"selfStretch":"fit","flexSize":null},"border":{"radius":"0px"}},"backgroundColor":"primary"} /-->
 
 <!-- wp:navigation {"textColor":"base","backgroundColor":"primary","overlayMenu":"always","icon":"menu","style":{"spacing":{"blockGap":"0"}}} /--></div>
