45b63cf45e59ec25cf6e52e2fd27fb81ace42b7b
schlotterer <joelschlotterer@gmail.com>
2024-01-22 11:16:57 -0600

    fix some asset enques after adding build

--- inc/scripts.php
+++ inc/scripts.php
@@ -34,10 +34,8 @@ function flexline_enqueue_style_sheet() {
  */
 function flexline_admin_enqueue_scripts() {
 	// Styles
-	wp_enqueue_style( 'flexline-base', get_template_directory_uri() . '/assets/css/base.css', array(), []);
-	//wp_enqueue_style( 'flexline-utilities', get_template_directory_uri() . '/assets/css/utilities.css', array(), []);
-	//wp_enqueue_style( 'flexline-templates', get_template_directory_uri() . '/assets/css/templates.css', array(), []);
-	//wp_register_style( 'flexline-swiper-styles', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css', [], '8.4.7' );
+	wp_enqueue_style( 'flexline-base', get_template_directory_uri() . '/assets/css/app.css', array(), []);
+	wp_enqueue_style( 'flexline-lightbox', get_template_directory_uri() . '/assets/css/lightbox.css', array(), []);
 }
 	
 add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\flexline_admin_enqueue_scripts' );
