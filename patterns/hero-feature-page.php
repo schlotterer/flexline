4b744e5f2de9a8be31698f4e4bcb40c0bd2bfa69
schlotterer <joelschlotterer@gmail.com>
2024-01-24 09:14:31 -0600

    add new hero pattern

---
+++ patterns/hero-feature-page.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Hero for feature pages.
+ * Slug: flexline/hero-feature-page
+ * Categories: featured
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"bottom":"0"}}},"layout":{"type":"constrained"},"metadata":{"name":"Feature Page Hero"}} -->
+<div class="wp-block-group alignfull" style="margin-bottom:0"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/placeholder-stringlights.gif'; ?>","dimRatio":80,"overlayColor":"primary","focalPoint":{"x":0.49,"y":0.2},"align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|x-small","padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|xxx-large","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xxx-large);padding-left:var(--wp--preset--spacing--medium)"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/placeholder-stringlights.gif'; ?>" style="object-position:49% 20%" data-object-fit="cover" data-object-position="49% 20%"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"bottom":"var:preset|spacing|xxx-large"}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignwide has-base-color has-text-color has-link-color" style="margin-bottom:var(--wp--preset--spacing--xxx-large)"><!-- wp:yoast-seo/breadcrumbs /--></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+<div class="wp-block-group"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"blockGap":"0"}},"textColor":"base","layout":{"type":"constrained","contentSize":"700px","wideSize":"1000px"},"fontSize":"x-small"} -->
+<div class="wp-block-group has-base-color has-text-color has-link-color has-x-small-font-size"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
+<h1 class="wp-block-heading has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400;text-transform:uppercase">H1 - Eyebrow - SEO Headline</h1>
+<!-- /wp:heading -->
+
+<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"max-60"} -->
+<h2 class="wp-block-heading has-text-align-center has-max-60-font-size" style="margin-top:0;margin-bottom:0">H2 - Creative Headline</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"right":"0","left":"0"}}},"fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size" style="margin-right:0;margin-left:0">Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"},"style":{"spacing":{"margin":{"top":"var:preset|spacing|small","bottom":"0"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--small);margin-bottom:0"><!-- wp:button {"textColor":"base","style":{"typography":{"textTransform":"uppercase"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"className":"is-style-outline","fontSize":"medium","enablePopup":true,"popupMediaURL":"https://www.youtube.com/watch?v=G1hKzCkywM8"} -->
+<div class="wp-block-button has-custom-font-size is-style-outline has-medium-font-size" style="text-transform:uppercase"><a class="wp-block-button__link has-base-color has-text-color has-link-color wp-element-button">Watch video</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div></div>
+<!-- /wp:cover --></div>
+<!-- /wp:group -->
