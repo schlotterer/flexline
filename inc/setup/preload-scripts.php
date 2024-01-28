658062a8d2db185fe0fea54acbf4ad24eef34061
schlotterer <joelschlotterer@gmail.com>
2024-01-27 19:38:44 -0600

    fix preloads

--- inc/setup/preload-scripts.php
+++ inc/setup/preload-scripts.php
@@ -13,13 +13,15 @@ namespace FlexLine\flexline;
  * @author WebDevStudios
  */
 function preload_scripts() {
+	/*  None of these really need preloading, leaving them here for reference.
 	?>
-	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/css/app.css" as="style"/>
-	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/css/templates.css" as="style"/>	
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/built/css/app.css" as="style"/>
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/built/css/template.css" as="style"/>	
 	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/css/customize.css" as="style"/>
 
-	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/js/global.js" as="script"/>
+	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/built/js/global.js" as="script"/>
 	<link rel="preload" href="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/assets/js/customize.js" as="script"/>
-	<?php
+	<?php   
+	*/
 }
 add_action( 'wp_head', __NAMESPACE__ . '\preload_scripts', 1 );
