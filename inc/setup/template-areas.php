1db88c50f722ea9f9cc8803cfeaf6100659f4891
schlotterer <joelschlotterer@gmail.com>
2024-01-27 23:21:02 -0600

    working on slide in nav

--- inc/setup/template-areas.php
+++ inc/setup/template-areas.php
@@ -12,22 +12,22 @@ add_filter( 'default_wp_template_part_areas', __NAMESPACE__ . '\flexline_templat
 function flexline_template_part_areas( array $areas ) {
         $areas[] = [
 
-        'area'        => 'topbar',
-        'area_tag'    => 'section',
-        'label'       => __( 'Top Bar', 'flexline' ),
-        'description' => __( 'Top Bar area for notifications', 'flexline' ),
-        'icon'        => 'tag'
+                'area'        => 'topbar',
+                'area_tag'    => 'section',
+                'label'       => __( 'Top Bar', 'flexline' ),
+                'description' => __( 'Top Bar area for notifications', 'flexline' ),
+                'icon'        => 'tag'
 
-    ];
-    $areas[] = [
-        
-            'area'        => 'slide-in-menu',
-            'area_tag'    => 'section',
-            'label'       => __( 'Slide In Menu', 'flexline' ),
-            'description' => __( 'Area for custom full window menu', 'flexline' ),
-            'icon'        => 'site'
-        
-    ];
+        ];
+        $areas[] = [
+                
+                'area'        => 'slideinmenu',
+                'area_tag'    => 'nav',
+                'label'       => __( 'Slide In Menu', 'flexline' ),
+                'description' => __( 'Area for custom full window menu', 'flexline' ),
+                'icon'        => 'site'
+                
+        ];
     
 	return $areas;
 }
