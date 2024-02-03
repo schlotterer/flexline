72a2b81c9705170e01003c1078ecd43a4041dd41
schlotterer <joelschlotterer@gmail.com>
2024-02-03 14:49:12 -0600

    more moduels and some spacing adjustments in blocks

---
+++ patterns/featured-text-poster-gallery.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Title: Text and Poster Gallery.
+ * Slug: flexline/text-poster-gallery
+ * Categories: flexline-modules
+ */
+?>
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"30px","bottom":"var:preset|spacing|x-large","left":"30px","top":"70px"},"margin":{"top":"0px"},"blockGap":"10px"}},"className":"welcome","layout":{"type":"constrained","wideSize":"","contentSize":""},"metadata":{"name":"Text and Poster Gallery"}} -->
+<div class="wp-block-group alignfull welcome" style="margin-top:0px;padding-top:70px;padding-right:30px;padding-bottom:var(--wp--preset--spacing--x-large);padding-left:30px"><!-- wp:columns {"verticalAlignment":"center","align":"wide"} -->
+<div class="wp-block-columns alignwide are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:heading {"textAlign":"left","style":{"typography":{"letterSpacing":"-1px"},"spacing":{"margin":{"bottom":"0px"}}},"className":"wp-block-heading","fontSize":"max-48"} -->
+<h2 class="wp-block-heading has-text-align-left has-max-48-font-size" style="margin-bottom:0px;letter-spacing:-1px">Experience the next generation of WordPress.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"left","fontSize":"large"} -->
+<p class="has-text-align-left has-large-font-size">FlexLine is the ultimate WordPress theme for website builders.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"center"} -->
+<div class="wp-block-column is-vertically-aligned-center"><!-- wp:gallery {"linkTo":"media","enablePosterGallery":true} -->
+<figure class="wp-block-gallery has-nested-images columns-default is-cropped"><!-- wp:image {"id":185,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large"><a href="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/featured-2.jpg'; ?>" alt="" class="wp-image-185"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":185,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large"><a href="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/featured-2.jpg'; ?>" alt="" class="wp-image-185"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":185,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large"><a href="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/featured-2.jpg'; ?>" alt="" class="wp-image-185"/></a></figure>
+<!-- /wp:image -->
+
+<!-- wp:image {"id":185,"sizeSlug":"large","linkDestination":"media"} -->
+<figure class="wp-block-image size-large"><a href="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/featured-2.jpg'; ?>" alt="" class="wp-image-185"/></a></figure>
+<!-- /wp:image --></figure>
+<!-- /wp:gallery --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
