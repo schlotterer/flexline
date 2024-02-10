d88fabc97ee58a3b4909c00926415a3e607e5ec0
schlotterer <joelschlotterer@gmail.com>
2024-02-10 01:14:00 -0600

    goodnight

--- patterns/section-info-boxes.php
+++ patterns/section-info-boxes.php
@@ -18,70 +18,7 @@
     </div>
     <!-- /wp:group -->
 
-    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
-    <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-            
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-        
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-        
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-            
-        </div>
-        <!-- /wp:column -->
-    </div>
-    <!-- /wp:columns -->
-
-    <!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
-    <div class="wp-block-columns alignwide"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-            
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0">
-        
-            <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
-            
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}},"border":{"width":"0px","style":"none"}}} -->
-        <div class="wp-block-column is-vertically-aligned-top" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"></div>
-        <!-- /wp:column -->
-    </div>
-    <!-- /wp:columns -->
+    <!-- wp:pattern {"slug":"flexline/info-boxes"} /-->
+    
 </div>
 <!-- /wp:group -->
