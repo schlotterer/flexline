da06eabce29294246fa58f72614420d595759277
schlotterer <joelschlotterer@gmail.com>
2024-02-17 11:01:44 -0600

    push

--- patterns/post-list-single-outline.php
+++ patterns/post-list-single-outline.php
@@ -10,7 +10,8 @@
 <!-- wp:group {"style":{"spacing":{"blockGap":"10px"}},"className":"is-style-outlined","layout":{"type":"constrained","justifyContent":"left"},"metadata":{"name":"Post List Single Text Outlined"}} -->
 <div class="wp-block-group is-style-outlined">
     <!-- wp:post-terms {"term":"category","style":{"typography":{"textTransform":"uppercase"}},"fontSize":"x-small","fontFamily":"display"} /-->
-    <!-- wp:post-title {"isLink":true} /-->
+    <!-- wp:post-title {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"},":hover":{"color":{"text":"var:preset|color|highlight"}}}}}} /-->
+    <!-- wp:post-excerpt {"showMoreOnNewLine":false,"excerptLength":32,"fontSize":"x-small"} /-->
     <!-- wp:read-more {"fontFamily":"display"} /-->
 </div>
 <!-- /wp:group -->
