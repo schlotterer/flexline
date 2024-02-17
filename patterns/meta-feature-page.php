829b085d05cb65254b2ada64ab49007836efe6cc
schlotterer <joelschlotterer@gmail.com>
2024-02-17 11:58:26 -0600

    detailing.

--- patterns/meta-feature-page.php
+++ patterns/meta-feature-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Feature Page Meta.
  * Slug: flexline/hero-feature-page
@@ -6,25 +7,31 @@
  */
 ?>
 <!-- wp:group {"align":"full","style":{"spacing":{"margin":{"bottom":"0"}}},"layout":{"type":"constrained"},"metadata":{"name":"Feature Page Meta"}} -->
-<div class="wp-block-group alignfull" style="margin-bottom:0"><!-- wp:cover {"url":"<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/placeholder-stringlights.gif'; ?>","dimRatio":80,"overlayColor":"primary","align":"full","style":{"spacing":{"blockGap":"var:preset|spacing|x-small","padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|xxx-large","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xxx-large);padding-left:var(--wp--preset--spacing--medium)"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url( get_theme_file_uri() ) . '/assets/images/placeholder-stringlights.gif'; ?>" style="" data-object-fit="cover"/><div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"bottom":"var:preset|spacing|xxx-large"}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-<div class="wp-block-group alignwide has-base-color has-text-color has-link-color" style="margin-bottom:var(--wp--preset--spacing--xxx-large)"><!-- wp:yoast-seo/breadcrumbs /--></div>
-<!-- /wp:group -->
+<div class="wp-block-group alignfull" style="margin-bottom:0"><!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()) . '/assets/images/placeholder-stringlights.gif'; ?>","id":659,"dimRatio":80,"overlayColor":"primary","minHeight":200,"align":"full","style":{"spacing":{"blockGap":"0","padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"margin":{"top":"0","bottom":"0"}},"color":{"duotone":"var:preset|duotone|primary"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium);min-height:200px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-659" alt="" src="<?php echo esc_url(get_theme_file_uri()) . '/assets/images/placeholder-stringlights.gif'; ?>" data-object-fit="cover" />
+        <div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"},":hover":{"color":{"text":"var:preset|color|highlight"}}}},"spacing":{"margin":{"bottom":"0"}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"},"fontSize":"x-small","fontFamily":"display"} -->
+            <div class="wp-block-group alignwide has-base-color has-text-color has-link-color has-display-font-family has-x-small-font-size" style="margin-bottom:0"><!-- wp:yoast-seo/breadcrumbs /--></div>
+            <!-- /wp:group -->
 
-<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
-<div class="wp-block-group"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"blockGap":"0"}},"textColor":"base","layout":{"type":"constrained","contentSize":"700px","wideSize":"1000px"},"fontSize":"x-small"} -->
-<div class="wp-block-group has-base-color has-text-color has-link-color has-x-small-font-size"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400"}},"fontSize":"small"} -->
-<h1 class="wp-block-heading has-text-align-center has-small-font-size" style="font-style:normal;font-weight:400;text-transform:uppercase">H1 - Eyebrow - SEO Headline</h1>
-<!-- /wp:heading -->
+            <!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|xxx-large","bottom":"var:preset|spacing|xxx-large"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"}} -->
+            <div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--xxx-large);margin-bottom:var(--wp--preset--spacing--xxx-large)"><!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"blockGap":"0"}},"textColor":"base","layout":{"type":"constrained","contentSize":"700px","wideSize":"1000px"},"fontSize":"x-small"} -->
+                <div class="wp-block-group has-base-color has-text-color has-link-color has-x-small-font-size"><!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"400"}},"className":"is-style-text-shadow","fontSize":"small","fontFamily":"display"} -->
+                    <h1 class="wp-block-heading has-text-align-center is-style-text-shadow has-display-font-family has-small-font-size" style="font-style:normal;font-weight:400;text-transform:uppercase">H1 - Eyebrow - SEO Headline</h1>
+                    <!-- /wp:heading -->
 
-<!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"fontSize":"max-60"} -->
-<h2 class="wp-block-heading has-text-align-center has-max-60-font-size" style="margin-top:0;margin-bottom:0">H2 - Creative Headline</h2>
-<!-- /wp:heading -->
+                    <!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"className":"is-style-text-shadow","fontSize":"max-60"} -->
+                    <h2 class="wp-block-heading has-text-align-center is-style-text-shadow has-max-60-font-size" style="margin-top:0;margin-bottom:0">H2 - Creative Headline</h2>
+                    <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"right":"0","left":"0"}}},"fontSize":"medium"} -->
-<p class="has-text-align-center has-medium-font-size" style="margin-right:0;margin-left:0">Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group --></div></div>
-<!-- /wp:cover --></div>
+                    <!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"right":"0","left":"0"}}},"fontSize":"medium"} -->
+                    <p class="has-text-align-center has-medium-font-size" style="margin-right:0;margin-left:0">Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                    <!-- /wp:paragraph -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+    </div>
+    <!-- /wp:cover -->
+</div>
 <!-- /wp:group -->
--- patterns/meta-page-seo.php
+++ patterns/meta-page-seo.php
@@ -6,19 +6,19 @@
  * Categories: flexline-heroes, flexline-sections
  */
 ?>
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"},"metadata":{"name":"Page Meta Bar SEO"}} -->
-<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0"><!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()) . '/assets/images/fallback.jpg'; ?>","dimRatio":80,"overlayColor":"primary","minHeight":50,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|xx-large","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"blockGap":"0"},"color":{"duotone":["#000000","#ffffff"]}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:var(--wp--preset--spacing--medium);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri()) . '/assets/images/fallback.jpg'; ?>" data-object-fit="cover" />
-        <div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"},"margin":{"bottom":"var:preset|spacing|large"}},"elements":{"link":{"color":{"text":"var:preset|color|neutral-light"},":hover":{"color":{"text":"var:preset|color|highlight"}}}}},"textColor":"base","layout":{"type":"default"},"fontSize":"x-small","fontFamily":"display"} -->
-            <div class="wp-block-group alignwide has-base-color has-text-color has-link-color has-display-font-family has-x-small-font-size" style="margin-bottom:var(--wp--preset--spacing--large);padding-right:0;padding-left:0"><!-- wp:yoast-seo/breadcrumbs /--></div>
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"0"}},"layout":{"type":"constrained"},"metadata":{"name":"Page Meta Bar SEO"}} -->
+<div class="wp-block-group alignfull" style="margin-top:0;margin-bottom:0"><!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri()) . '/assets/images/fallback.jpg'; ?>","id":509,"dimRatio":80,"overlayColor":"primary","focalPoint":{"x":0.47,"y":0.91},"minHeight":50,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|xx-large","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"blockGap":"0","margin":{"top":"0","bottom":"0"}},"color":{"duotone":"var:preset|duotone|primary"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--xx-large);padding-left:var(--wp--preset--spacing--medium);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-509" alt="" src="<?php echo esc_url(get_theme_file_uri()) . '/assets/images/fallback.jpg'; ?>" style="object-position:47% 91%" data-object-fit="cover" data-object-position="47% 91%" />
+        <div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"},"margin":{"bottom":"var:preset|spacing|large"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"},"fontSize":"x-small"} -->
+            <div class="wp-block-group alignwide has-base-color has-text-color has-link-color has-x-small-font-size" style="margin-bottom:var(--wp--preset--spacing--large);padding-right:0;padding-left:0"><!-- wp:yoast-seo/breadcrumbs /--></div>
             <!-- /wp:group -->
 
-            <!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"300"}},"fontSize":"small"} -->
-            <h1 class="wp-block-heading has-text-align-center has-small-font-size" style="font-style:normal;font-weight:300;text-transform:uppercase">H1 SEO Headline</h1>
+            <!-- wp:heading {"textAlign":"center","level":1,"style":{"typography":{"textTransform":"uppercase","fontStyle":"normal","fontWeight":"300"}},"className":"is-style-text-shadow","fontSize":"small","fontFamily":"display"} -->
+            <h1 class="wp-block-heading has-text-align-center is-style-text-shadow has-display-font-family has-small-font-size" style="font-style:normal;font-weight:300;text-transform:uppercase">H1 SEO Headline</h1>
             <!-- /wp:heading -->
 
-            <!-- wp:heading {"textAlign":"center","fontSize":"max-36"} -->
-            <h2 class="wp-block-heading has-text-align-center has-max-36-font-size">H2 Creative Headline</h2>
+            <!-- wp:heading {"textAlign":"center","className":"is-style-text-shadow","fontSize":"max-36"} -->
+            <h2 class="wp-block-heading has-text-align-center is-style-text-shadow has-max-36-font-size">H2 Creative Headline</h2>
             <!-- /wp:heading -->
         </div>
     </div>
