678140b45a62805d74aef4d90021acbbbc3f604c
schlotterer <joelschlotterer@gmail.com>
2024-02-18 00:10:26 -0600

    events template

--- patterns/event-single-content.php
+++ patterns/event-single-content.php
@@ -7,9 +7,9 @@
  */
 ?>
 <!-- wp:group {"layout":{"type":"constrained"},"metadata":{"name":"Event Single Wrap"}} -->
-<div class="wp-block-group"><!-- wp:columns {"align":"wide"} -->
-    <div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%"} -->
-        <div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:tec/single-event /--></div>
+<div class="wp-block-group"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"66.66%","style":{"spacing":{"blockGap":"var:preset|spacing|small","padding":{"right":"0","left":"0"}}}} -->
+        <div class="wp-block-column" style="padding-right:0;padding-left:0;flex-basis:66.66%"><!-- wp:post-content /--></div>
         <!-- /wp:column -->
 
         <!-- wp:column {"width":"33.33%"} -->
@@ -19,5 +19,3 @@
     <!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
-
-   
--- patterns/meta-event-single.php
+++ patterns/meta-event-single.php
@@ -12,13 +12,13 @@
         <div class="wp-block-cover__inner-container"><!-- wp:cover {"useFeaturedImage":true,"dimRatio":80,"overlayColor":"primary","minHeight":50,"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"blockGap":"var:preset|spacing|small"},"color":{"duotone":"var:preset|duotone|primary"}},"layout":{"type":"constrained"}} -->
             <div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--medium);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span>
                 <div class="wp-block-cover__inner-container"><!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"0","left":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|neutral-light"},":hover":{"color":{"text":"var:preset|color|highlight"}}}}},"textColor":"neutral-light","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"top"},"fontSize":"x-small","fontFamily":"display"} -->
-                    <div class="wp-block-group alignwide has-neutral-light-color has-text-color has-link-color has-display-font-family has-x-small-font-size" style="padding-right:0;padding-left:0"><!-- wp:post-terms {"term":"category"} /--></div>
+                    <div class="wp-block-group alignwide has-neutral-light-color has-text-color has-link-color has-display-font-family has-x-small-font-size" style="padding-right:0;padding-left:0"><!-- wp:yoast-seo/breadcrumbs /--></div>
                     <!-- /wp:group -->
 
                     <!-- wp:post-title {"textAlign":"left","level":1,"style":{"typography":{"textTransform":"none"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","fontSize":"max-48"} /-->
 
-                    <!-- wp:group {"style":{"spacing":{"blockGap":"0"},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
-                    <div class="wp-block-group has-base-color has-text-color has-link-color"><!-- wp:tribe/event-datetime /--></div>
+                    <!-- wp:group {"style":{"spacing":{"blockGap":"0"},"elements":{"link":{"color":{"text":"var:preset|color|neutral-light"},":hover":{"color":{"text":"var:preset|color|highlight"}}}}},"textColor":"neutral-light","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"fontSize":"x-small","fontFamily":"display"} -->
+                    <div class="wp-block-group has-neutral-light-color has-text-color has-link-color has-display-font-family has-x-small-font-size"><!-- wp:post-terms {"term":"category"} /--></div>
                     <!-- /wp:group -->
                 </div>
             </div>
