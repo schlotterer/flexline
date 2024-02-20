fed5bb0ae1c95c9a54929ef727df2379c4b7e412
schlotterer <joelschlotterer@gmail.com>
2024-02-19 18:30:18 -0600

    see about horizontal scroll in admin

--- patterns/section-latest-posts.php
+++ patterns/section-latest-posts.php
@@ -6,8 +6,8 @@
  * Categories: flexline-sections, flexline-modules, flexline-posts
  */
 ?>
-<!-- wp:group {"align":"full","className":"latests-posts-3-card","layout":{"type":"constrained"},"metadata":{"name":"Latests Posts - 3 card"}} -->
-<div class="wp-block-group alignfull latests-posts-3-card"><!-- wp:heading {"textAlign":"center"} -->
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|medium","left":"var:preset|spacing|medium"}}},"className":"latests-posts-3-card","layout":{"type":"constrained"},"metadata":{"name":"Latests Posts - 3 card"}} -->
+<div class="wp-block-group alignfull latests-posts-3-card" style="padding-right:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:heading {"textAlign":"center"} -->
     <h2 class="wp-block-heading has-text-align-center">Latest Posts</h2>
     <!-- /wp:heading -->
 
@@ -15,7 +15,7 @@
     <div class="wp-block-query alignwide">
         <!-- wp:post-template {"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|medium"}},"layout":{"type":"grid","columnCount":3}} -->
         
-         <!-- wp:pattern {"slug":"flexline/post-grid-single-card-alt"} /-->
+        <!-- wp:pattern {"slug":"flexline/post-grid-single-card-alt"} /-->
 
         <!-- /wp:post-template -->
 
@@ -28,5 +28,3 @@
     <!-- /wp:query -->
 </div>
 <!-- /wp:group -->
-
-
