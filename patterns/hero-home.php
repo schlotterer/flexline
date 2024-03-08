8bc9477db6cfcc3868013dee61893bcf97af20d9
schlotterer <joelschlotterer@gmail.com>
2024-03-08 10:22:04 -0600

    make it universal

--- patterns/hero-home.php
+++ patterns/hero-home.php
@@ -40,12 +40,6 @@
         </div>
     </div>
     <!-- /wp:cover -->
-
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","verticalAlignment":"top"},"style":{"spacing":{"margin":{"top":"0px","bottom":"0px"}}}} -->
-    <div class="wp-block-buttons" style="margin-top:0px;margin-bottom:0px"><!-- wp:button {"backgroundColor":"secondary","style":{"border":{"radius":"100px"},"spacing":{"padding":{"left":"var:preset|spacing|large","right":"var:preset|spacing|large","top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"className":"scrollTo animated bounce delay-2s slow"} -->
-        <div class="wp-block-button scrollTo animated bounce delay-2s slow" id="scrollTo"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" href="#scrollTo" style="border-radius:100px;padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--large)">↓</a></div>
-        <!-- /wp:button -->
-    </div>
-    <!-- /wp:buttons -->
+    <!-- wp:pattern {"slug":"flexline/button-scroll-to"} /-->
 </div>
 <!-- /wp:group -->
