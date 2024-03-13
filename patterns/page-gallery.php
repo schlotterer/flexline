a23a65a5b5d3c2abdb436ceb3877a699589c6f13
schlotterer <joelschlotterer@gmail.com>
2024-03-13 09:53:04 -0500

    missed one

--- patterns/page-gallery.php
+++ patterns/page-gallery.php
@@ -1,22 +1,7 @@
 <?php
 /**
- * Title: Sample top level page.
- * Slug: flexline/page-feature-hub
+ * Title: Gallery Page
+ * Slug: flexline/page-gallery
  * Categories: flexline-page
  */
 ?>
-<!-- wp:pattern {"slug":"flexline/hero-feature-page"} /-->
-<!-- wp:pattern {"slug":"flexline/text-video"} /-->
-<!-- wp:pattern {"slug":"flexline/section-page-links"} /-->
-<!-- wp:pattern {"slug":"flexline/testimonial-card-light"} /-->
-<!-- wp:separator {"className":"is-style-dots"} -->
-<hr class="wp-block-separator has-alpha-channel-opacity is-style-dots"/>
-<!-- /wp:separator -->
-<!-- wp:heading {"textAlign":"center","fontSize":"max-36"} -->
-<h2 class="wp-block-heading has-text-align-center has-max-36-font-size">Frequently Asked Questions</h2>
-<!-- /wp:heading -->
-<!-- wp:pattern {"slug":"flexline/tabs-accordion"} /-->
-<!-- wp:separator {"className":"is-style-dots"} -->
-<hr class="wp-block-separator has-alpha-channel-opacity is-style-dots"/>
-<!-- /wp:separator -->
-<!-- wp:pattern {"slug":"flexline/cta-large"} /-->
