22c42a26c2f72fc17cd10b8c53dab4e5174ac017
schlotterer <joelschlotterer@gmail.com>
2024-03-26 15:14:40 -0500

    make this something more

--- patterns/cta-full-width.php
+++ patterns/cta-full-width.php
@@ -6,10 +6,10 @@
  * Categories: flexline-components, flexline-cta,
  */
 ?>
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}}},"backgroundColor":"primary","textColor":"base","className":"is-style-default","layout":{"type":"constrained"},"metadata":{"name":"CTA - Full Width"}} -->
-<div class="wp-block-group alignfull is-style-default has-base-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:columns {"verticalAlignment":"center","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|small"}}}} -->
-    <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","width":""} -->
-        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:group {"style":{"border":{"width":"0px","style":"none"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"},"metadata":{"name":"CTA - Basic - Row"}} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|xx-large","right":"var:preset|spacing|xx-large"}},"position":{"type":"sticky","top":"0px"}},"backgroundColor":"primary","textColor":"base","className":"is-style-default","layout":{"type":"constrained"},"metadata":{"name":"CTA - Full Width"}} -->
+<div class="wp-block-group alignfull is-style-default has-base-color has-primary-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--xx-large);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--xx-large)"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"style":{"spacing":{"blockGap":{"top":"0","left":"var:preset|spacing|x-small"},"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small"},"margin":{"top":"0","bottom":"0"}}}} -->
+    <div class="wp-block-columns are-vertically-aligned-center is-not-stacked-on-mobile" style="margin-top:0;margin-bottom:0;padding-top:0;padding-right:var(--wp--preset--spacing--x-small);padding-bottom:0;padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:column {"verticalAlignment":"center","width":"","style":{"spacing":{"blockGap":"0","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:group {"style":{"border":{"width":"0px","style":"none"},"spacing":{"padding":{"top":"0","right":"0","bottom":"0","left":"0"},"blockGap":"0"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"center"},"metadata":{"name":"CTA - Basic - Row"}} -->
             <div class="wp-block-group" style="border-style:none;border-width:0px;padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.5"},"layout":{"selfStretch":"fixed","flexSize":"720px"}},"fontSize":"large"} -->
                 <p class="has-large-font-size" style="line-height:1.5">This CTA creates a full width color band for breaking up content. </p>
                 <!-- /wp:paragraph -->
@@ -18,8 +18,8 @@
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"center","width":"250px"} -->
-        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:250px"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"left"}} -->
+        <!-- wp:column {"verticalAlignment":"center","width":"250px","style":{"spacing":{"blockGap":"0","padding":{"top":"0","bottom":"0","left":"0","right":"0"}}}} -->
+        <div class="wp-block-column is-vertically-aligned-center" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0;flex-basis:250px"><!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
             <div class="wp-block-buttons"><!-- wp:button {"fontSize":"small"} -->
                 <div class="wp-block-button has-custom-font-size has-small-font-size"><a class="wp-block-button__link wp-element-button">Get Started Today</a></div>
                 <!-- /wp:button -->
