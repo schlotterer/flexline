4f4b8b4cf84a31a016992dc70902439f03fc0091
schlotterer <joelschlotterer@gmail.com>
2024-03-29 12:20:32 -0500

    add new footer pattern

---
+++ patterns/footer-phone-directory-links.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Title: Footer with Phone Directory and links
+ * Slug: flexline/footer-phone-directory-links
+ * Categories: flexline-footers
+ * Block Types: core/template-part/footer
+ */
+?>
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"margin":{"top":"0px"}}},"backgroundColor":"contrast","textColor":"base","className":"has-background-color","layout":{"type":"constrained"},"fontSize":"small","metadata":{"name":"Footer Phone Directory and Links"}} -->
+<div class="wp-block-group alignfull has-background-color has-base-color has-contrast-background-color has-text-color has-background has-link-color has-small-font-size" style="margin-top:0px"><!-- wp:cover {"dimRatio":90,"overlayColor":"contrast","align":"full","style":{"color":{"duotone":"var:preset|duotone|neutral-dark"}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-cover alignfull"><span aria-hidden="true" class="wp-block-cover__background has-contrast-background-color has-background-dim-90 has-background-dim"></span>
+        <div class="wp-block-cover__inner-container"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":[]}},"spacing":{"blockGap":{"left":"var:preset|spacing|x-small"}}}} -->
+            <div class="wp-block-columns alignwide has-link-color"><!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"className":"is-justify-content-center-mobile"} -->
+                <div class="wp-block-column is-justify-content-center-mobile" style="flex-basis:25%"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"className":"has-text-align-center","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"}} -->
+                    <div class="wp-block-group has-text-align-center"><!-- wp:site-logo {"width":200,"style":{"color":{"duotone":["#ffffff","#ffffff"]}}} /-->
+
+                        <!-- wp:shortcode -->
+                        [flexline_contact_info]
+                        <!-- /wp:shortcode -->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:column -->
+
+                <!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+                <div class="wp-block-column" style="flex-basis:25%" id="phoneDirectory"><!-- wp:heading {"textAlign":"center"} -->
+                    <h2 class="wp-block-heading has-text-align-center">Phone Directory</h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph {"align":"center"} -->
+                    <p class="has-text-align-center">General: <a href="tel:19998887777">(999) 888-7777</a></p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph {"align":"center"} -->
+                    <p class="has-text-align-center">General: <a href="tel:19998887777">(999) 888-7777</a></p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:paragraph {"align":"center"} -->
+                    <p class="has-text-align-center">General: <a href="tel:19998887777">(999) 888-7777</a></p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+                    <div class="wp-block-buttons"><!-- wp:button {"enablePopup":true} -->
+                        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/schedule-a-tour-modal/">Schedule a tour</a></div>
+                        <!-- /wp:button -->
+                    </div>
+                    <!-- /wp:buttons -->
+                </div>
+                <!-- /wp:column -->
+
+                <!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+                <div class="wp-block-column" style="flex-basis:25%"><!-- wp:heading {"textAlign":"center"} -->
+                    <h2 class="wp-block-heading has-text-align-center">Explore</h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:navigation {"ref":93,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}}} /-->
+                </div>
+                <!-- /wp:column -->
+
+                <!-- wp:column {"width":"25%","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+                <div class="wp-block-column" style="flex-basis:25%"><!-- wp:heading {"textAlign":"center"} -->
+                    <h2 class="wp-block-heading has-text-align-center">Learn</h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:navigation {"ref":95,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}}} /-->
+
+                    <!-- wp:social-links {"iconColor":"contrast","iconColorValue":"#000","iconBackgroundColor":"base","iconBackgroundColorValue":"#fff","style":{"spacing":{"blockGap":"10px"}},"className":"is-style-default","layout":{"type":"flex","justifyContent":"center"}} -->
+                    <ul class="wp-block-social-links has-icon-color has-icon-background-color is-style-default"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+                        <!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+                        <!-- wp:social-link {"url":"#","service":"twitter"} /-->
+                    </ul>
+                    <!-- /wp:social-links -->
+                </div>
+                <!-- /wp:column -->
+            </div>
+            <!-- /wp:columns -->
+        </div>
+    </div>
+    <!-- /wp:cover -->
+</div>
+<!-- /wp:group -->
