1522409532c9ff6adcb6b558ea61e1a43a52f578
schlotterer <joelschlotterer@gmail.com>
2024-04-06 20:14:13 -0500

    Fix call links

--- patterns/button-call.php
+++ patterns/button-call.php
@@ -6,12 +6,15 @@
  * Categories: flexline-utilities, flexline-components, flexline-cta
  */
 // Retrieve the phone number from the customizer settings
-$phone_number = get_theme_mod('flexline_main_phone_number', '');
-$tel_link_number = preg_replace('/\D+/', '', $phone_number);
+namespace FlexLine\flexline;
+$mainPhoneTitle = get_theme_mod('flexline_main_phone_title', '');
+$phoneLinkText = !empty($mainPhoneTitle) ? $mainPhoneTitle : "Call Now";
+$aria_label = !empty($mainPhoneTitle) ? $mainPhoneTitle : "Call Now";
+$tel_link_number = flexline_get_phone_button_link();
 ?>
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
 <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","style":{"spacing":{"padding":{"left":"var:preset|spacing|large","right":"var:preset|spacing|large","top":"var:preset|spacing|small","bottom":"var:preset|spacing|small"}}},"className":"is-style-fill"} -->
-    <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" href="tel:<?php echo $tel_link_number; ?>" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--large)">Call Now</a></div>
+    <div class="wp-block-button is-style-fill"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" aria-label="<?php echo $aria_label; ?>" href="<?php echo $tel_link_number; ?>" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--large)"><?php echo $phoneLinkText; ?></a></div>
     <!-- /wp:button -->
 </div>
 <!-- /wp:buttons -->
--- patterns/slide-in-menu.php
+++ patterns/slide-in-menu.php
@@ -10,43 +10,95 @@
  * 
  */
 // Retrieve the phone number from the customizer settings
-$phone_number = get_theme_mod('flexline_main_phone_number', '');
-$tel_link_number = preg_replace('/\D+/', '', $phone_number);
+namespace FlexLine\flexline;
+$mainPhoneTitle = get_theme_mod('flexline_main_phone_title', '');
+$phoneLinkText = !empty($mainPhoneTitle) ? $mainPhoneTitle : "Call Now";
+$aria_label = !empty($mainPhoneTitle) ? $mainPhoneTitle : "Call Now";
+$tel_link_number = flexline_get_phone_button_link();
 ?>
-<!-- wp:group {"tagName":"aside","lock":{"move":true,"remove":true},"style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}}},"backgroundColor":"primary","layout":{"type":"constrained"},"metadata":{"name":"Slide In Menu"}} -->
-<aside id="slide-in-menu" class="wp-block-group has-primary-background-color has-background" style="padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--small)"><!-- wp:group {"layout":{"type":"constrained"},"metadata":{"name":"Logo and Feature Buttons"}} -->
-    <div class="wp-block-group"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"},"metadata":{"name":"Logo, Search and Feature Buttons"}} -->
-        <div class="wp-block-group"><!-- wp:site-logo {"width":250,"style":{"color":{"duotone":["#ffffff","#ffffff"]}}} /-->
+<!-- wp:group {"tagName":"aside","lock":{"move":false,"remove":false},"metadata":{"name":"Slide In Menu"},"backgroundColor":"primary","layout":{"type":"constrained"}} -->
+<aside id="slide-in-menu" class="wp-block-group has-primary-background-color has-background"><!-- wp:cover {"overlayColor":"primary","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"vh","align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"constrained"}} -->
+    <div class="wp-block-cover alignfull" style="margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--x-large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--x-large);padding-left:var(--wp--preset--spacing--small);min-height:100vh"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-100 has-background-dim"></span>
+        <div class="wp-block-cover__inner-container"><!-- wp:group {"metadata":{"name":"Logo and Feature Buttons"},"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group" style="margin-top:0;margin-bottom:0"><!-- wp:group {"metadata":{"name":"Logo, Search and Feature Buttons"},"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"center"}} -->
+                <div class="wp-block-group"><!-- wp:site-logo {"width":200,"style":{"color":{"duotone":["#ffffff","#ffffff"]}}} /-->
 
-            <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-            <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"secondary","style":{"spacing":{"padding":{"left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}}} -->
-                <div class="wp-block-button"><a class="wp-block-button__link has-secondary-background-color has-background wp-element-button" href="/contact/" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--x-large)">Contact</a></div>
-                <!-- /wp:button -->
+                    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+                    <div class="wp-block-buttons"><!-- wp:button {"style":{"spacing":{"padding":{"left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}}}} -->
+                        <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="/contact/" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--x-large)">Contact</a></div>
+                        <!-- /wp:button -->
 
-                <!-- wp:button {"textColor":"base","style":{"spacing":{"padding":{"left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"className":"is-style-outline","hideOnDesktop":true,"hideOnTablet":true} -->
-                <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-base-color has-text-color has-link-color wp-element-button" href="tel:<?php echo $tel_link_number; ?>" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--x-large)">Call Now</a></div>
-                <!-- /wp:button -->
+                        <!-- wp:button {"textColor":"base","style":{"spacing":{"padding":{"left":"var:preset|spacing|x-large","right":"var:preset|spacing|x-large","top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"className":"is-style-outline","hideOnDesktop":true,"hideOnTablet":true} -->
+                        <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-base-color has-text-color has-link-color wp-element-button" href="<?php echo $tel_link_number; ?>" aria-label="<?php echo $aria_label; ?>" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--x-large)"><?php echo $phoneLinkText; ?></a></div>
+                        <!-- /wp:button -->
+                    </div>
+                    <!-- /wp:buttons -->
+
+                    <!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","style":{"layout":{"selfStretch":"fill","flexSize":null},"border":{"radius":{"topRight":"5px","bottomLeft":"5px","bottomRight":"15px","topLeft":"15px"}}}} /-->
+                </div>
+                <!-- /wp:group -->
             </div>
-            <!-- /wp:buttons -->
+            <!-- /wp:group -->
 
-            <!-- wp:search {"label":"Search","showLabel":false,"width":100,"widthUnit":"%","buttonText":"Search","style":{"layout":{"selfStretch":"fill","flexSize":null}}} /-->
+            <!-- wp:group {"metadata":{"name":"Menu Columns"},"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"blockGap":"var:preset|spacing|small"}},"textColor":"base","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group has-base-color has-text-color has-link-color"><!-- wp:columns {"verticalAlignment":"top"} -->
+                <div class="wp-block-columns are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top"} -->
+                    <div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":76,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontFamily":"display"} /--></div>
+                    <!-- /wp:column -->
+
+                    <!-- wp:column {"verticalAlignment":"top"} -->
+                    <div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":77,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontFamily":"display"} /--></div>
+                    <!-- /wp:column -->
+
+                    <!-- wp:column {"verticalAlignment":"top"} -->
+                    <div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":78,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontFamily":"display"} /--></div>
+                    <!-- /wp:column -->
+                </div>
+                <!-- /wp:columns -->
+
+                <!-- wp:columns {"verticalAlignment":"top"} -->
+                <div class="wp-block-columns are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top"} -->
+                    <div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":79,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontFamily":"display"} /--></div>
+                    <!-- /wp:column -->
+
+                    <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
+                    <div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":80,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"0"}},"fontFamily":"display"} /-->
+
+                        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+                        <div class="wp-block-buttons"><!-- wp:button -->
+                            <div class="wp-block-button"><a class="wp-block-button__link wp-element-button" href="https://bravo.websitesforseniorliving.com/careers/">Careers</a></div>
+                            <!-- /wp:button -->
+                        </div>
+                        <!-- /wp:buttons -->
+                    </div>
+                    <!-- /wp:column -->
+
+                    <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}}} -->
+                    <div class="wp-block-column is-vertically-aligned-top"><!-- wp:navigation {"ref":98,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"center"},"style":{"spacing":{"blockGap":"var:preset|spacing|x-small"}},"fontFamily":"display"} /-->
+
+                        <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+                        <div class="wp-block-buttons"><!-- wp:button {"textColor":"alternate","style":{"elements":{"link":{"color":{"text":"var:preset|color|alternate"}}}},"className":"is-style-outline"} -->
+                            <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-alternate-color has-text-color has-link-color wp-element-button" href="https://charlie.websitesforseniorliving.com/schedule-a-tour">Schedule a Tour</a></div>
+                            <!-- /wp:button -->
+                        </div>
+                        <!-- /wp:buttons -->
+
+                        <!-- wp:social-links {"iconColor":"neutral-light","iconColorValue":"#FEFAE0","style":{"spacing":{"blockGap":{"left":"var:preset|spacing|x-small"}}},"className":"is-style-logos-only","layout":{"type":"flex","justifyContent":"center"}} -->
+                        <ul class="wp-block-social-links has-icon-color is-style-logos-only"><!-- wp:social-link {"url":"#","service":"facebook"} /-->
+
+                            <!-- wp:social-link {"url":"#","service":"instagram"} /-->
+
+                            <!-- wp:social-link {"url":"#","service":"linkedin"} /-->
+                        </ul>
+                        <!-- /wp:social-links -->
+                    </div>
+                    <!-- /wp:column -->
+                </div>
+                <!-- /wp:columns -->
+            </div>
+            <!-- /wp:group -->
         </div>
-        <!-- /wp:group -->
     </div>
-    <!-- /wp:group -->
-
-    <!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base","layout":{"type":"constrained"},"metadata":{"name":"Menu Columns"}} -->
-    <div class="wp-block-group has-base-color has-text-color has-link-color"><!-- wp:columns -->
-        <div class="wp-block-columns"><!-- wp:column -->
-            <div class="wp-block-column"><!-- wp:navigation {"ref":427,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"},"style":{"spacing":{"blockGap":"0"}}} /--></div>
-            <!-- /wp:column -->
-
-            <!-- wp:column -->
-            <div class="wp-block-column"><!-- wp:navigation {"ref":428,"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"},"style":{"spacing":{"blockGap":"0"}}} /--></div>
-            <!-- /wp:column -->
-        </div>
-        <!-- /wp:columns -->
-    </div>
-    <!-- /wp:group -->
+    <!-- /wp:cover -->
 </aside>
 <!-- /wp:group -->
