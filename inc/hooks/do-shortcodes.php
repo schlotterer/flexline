5943607b4ff17b219c514926da4434f5cae4c8b2
schlotterer <joelschlotterer@gmail.com>
2024-04-14 09:36:19 -0500

    try this

--- inc/hooks/do-shortcodes.php
+++ inc/hooks/do-shortcodes.php
@@ -15,13 +15,13 @@ namespace FlexLine\flexline;
  * 
  */
 // Activate shortcode function in Post Title.
-add_filter( 'the_title', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'single_post_title', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_title', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_metadesc', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_opengraph_title', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_opengraph_desc', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_opengraph_site_name', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_twitter_title', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'wpseo_twitter_description', __NAMESPACE__ . '\do_shortcode' );
-add_filter( 'the_excerpt', __NAMESPACE__ . '\do_shortcode' );
+add_filter( 'the_title', 'do_shortcode' );
+add_filter( 'single_post_title', 'do_shortcode' );
+add_filter( 'wpseo_title', 'do_shortcode' );
+add_filter( 'wpseo_metadesc', 'do_shortcode' );
+add_filter( 'wpseo_opengraph_title', 'do_shortcode' );
+add_filter( 'wpseo_opengraph_desc', 'do_shortcode' );
+add_filter( 'wpseo_opengraph_site_name', 'do_shortcode' );
+add_filter( 'wpseo_twitter_title', 'do_shortcode' );
+add_filter( 'wpseo_twitter_description', 'do_shortcode' );
+add_filter( 'the_excerpt', 'do_shortcode' );
