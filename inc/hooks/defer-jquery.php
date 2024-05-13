053561431d810dc438e6c5267d99313a9f8ae29d
schlotterer <joelschlotterer@gmail.com>
2024-05-13 09:08:33 -0500

    defer jquery

---
+++ inc/hooks/defer-jquery.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Defer jQuery.
+ *
+ * @package flexline
+ */
+
+namespace FlexLine\flexline;
+
+/**
+ * Defer jQuery.
+ *
+ * @author Joel Schlotterer
+ *
+ */
+function defer_jquery() {
+    if (!is_admin()) {
+      wp_deregister_script('jquery');
+      wp_register_script('jquery', '/js/jquery/jquery.js', [], null, [ 'strategy'  => 'defer']);
+      wp_enqueue_script('jquery');
+    }
+  }
+  add_action('wp_enqueue_scripts', __NAMESPACE__ . '\defer_jquery',100);
