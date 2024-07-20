a6c2f60f9fa9cf605ef80baa6228fe57f22e1324
schlotterer <joelschlotterer@gmail.com>
2024-07-20 15:14:22 -0500

    fix cat name

--- patterns/header-centered-light-on-dark.php
+++ patterns/header-centered-light-on-dark.php
@@ -2,11 +2,12 @@
 
 /**
  * Title: Header with Centered logo Light on Dark.
- * Slug: flexline/header-centered-light-on-dark/
+ * Slug: flexline/header-centered-light-on-dark
  * Categories: flexline-headers
  * Block Types: core/template-part/header
  */
 ?>
+
 <!-- wp:group {"metadata":{"name":"Header Centered - Light over Dark"},"className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|large","right":"var:preset|spacing|large"},"margin":{"top":"0"}}},"gradient":"dark-transparent-top-to-bottom","layout":{"type":"constrained"}} -->
 <div class="wp-block-group is-style-default has-dark-transparent-top-to-bottom-gradient-background has-background" style="margin-top:0;padding-top:0;padding-right:var(--wp--preset--spacing--large);padding-bottom:0;padding-left:var(--wp--preset--spacing--large)"><!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"2px","bottom":"2px","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"},"margin":{"top":"0","bottom":"0"}},"border":{"top":{"width":"0px","style":"none"},"right":{"width":"0px","style":"none"},"bottom":{"color":"var:preset|color|secondary","width":"1px"},"left":{"width":"0px","style":"none"}}},"gradient":"primary-primaryDark","layout":{"type":"constrained"},"hideOnMobile":true} -->
     <div class="wp-block-group alignfull has-primary-primaryDark-gradient-background has-background" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:var(--wp--preset--color--secondary);border-bottom-width:1px;border-left-style:none;border-left-width:0px;margin-top:0;margin-bottom:0;padding-top:2px;padding-right:var(--wp--preset--spacing--medium);padding-bottom:2px;padding-left:var(--wp--preset--spacing--medium)"><!-- wp:navigation {"ref":75,"showSubmenuIcon":false,"overlayMenu":"never","overlayBackgroundColor":"primary","overlayTextColor":"neutral-light","className":"is-style-light-over-dark","style":{"spacing":{"blockGap":"var:preset|spacing|small"},"typography":{"textTransform":"uppercase"}},"fontSize":"x-small","fontFamily":"display","layout":{"type":"flex","justifyContent":"center"},"hideOnTablet":true,"hideOnMobile":true} /--></div>
