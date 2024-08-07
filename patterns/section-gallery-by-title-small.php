de8e96c9f4afa8113c4bd5dc1825b4e4692f62f4
schlotterer <joelschlotterer@gmail.com>
2024-08-06 23:49:34 -0500

    update special gallery layouts with grid

--- patterns/section-gallery-by-title-small.php
+++ patterns/section-gallery-by-title-small.php
@@ -8,12 +8,12 @@
 
 namespace FlexLine\flexline;
 ?>
-<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"backgroundColor":"neutral","className":"is-style-card","layout":{"type":"constrained"},"metadata":{"name":"Gallery by Title Small"}} -->
-<div class="wp-block-group alignwide is-style-card has-neutral-background-color has-background" style="padding-top:0;padding-bottom:0"><!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"left":"0"}}}} -->
-    <div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"stretch","width":"35%"} -->
-        <div class="wp-block-column is-vertically-aligned-stretch" style="flex-basis:35%"><!-- wp:cover {"url":"<?php echo esc_url(feature_image_fallback()); ?>","id":356,"dimRatio":80,"overlayColor":"primary","minHeight":100,"minHeightUnit":"%","style":{"color":{"duotone":"var:preset|duotone|primary"},"spacing":{"padding":{"top":"var:preset|spacing|xxx-large","bottom":"var:preset|spacing|xxx-large","left":"var:preset|spacing|large","right":"var:preset|spacing|large"}}},"layout":{"type":"constrained"}} -->
-            <div class="wp-block-cover" style="padding-top:var(--wp--preset--spacing--xxx-large);padding-right:var(--wp--preset--spacing--large);padding-bottom:var(--wp--preset--spacing--xxx-large);padding-left:var(--wp--preset--spacing--large);min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-356" alt="Sample Image" src="<?php echo esc_url(feature_image_fallback()); ?>" data-object-fit="cover" />
-                <div class="wp-block-cover__inner-container"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"className":"is-style-text-shadow","fontSize":"max-48"} -->
+<!-- wp:group {"metadata":{"name":"Gallery Next Title Small","categories":["flexline-modules"],"patternName":"flexline/section-gallery-by-title-small"},"align":"wide","className":"is-style-card","style":{"spacing":{"padding":{"top":"0","bottom":"0"}}},"backgroundColor":"neutral-light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide is-style-card has-neutral-light-background-color has-background" style="padding-top:0;padding-bottom:0"><!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"top":"0","left":"0"}}},"stackAtTablet":true} -->
+    <div class="wp-block-columns alignfull"><!-- wp:column {"verticalAlignment":"stretch","width":"350px","metadata":{"name":"Text Column"},"style":{"spacing":{"blockGap":"var:preset|spacing|medium"}}} -->
+        <div class="wp-block-column is-vertically-aligned-stretch" style="flex-basis:350px"><!-- wp:cover {"url":"https://alpha.test/wp-content/uploads/2024/05/charlie-0.0.008-pexels-suzyhazelwood-2495651-jpg.webp","id":356,"alt":"Sample Image","dimRatio":80,"overlayColor":"primary","isUserOverlayColor":true,"minHeight":100,"minHeightUnit":"%","metadata":{"name":"Background Image"},"style":{"color":{"duotone":"var:preset|duotone|primary"},"spacing":{"padding":{"top":"var:preset|spacing|xxx-large","bottom":"var:preset|spacing|xxx-large","left":"var:preset|spacing|small","right":"var:preset|spacing|small"},"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
+            <div class="wp-block-cover" style="padding-top:var(--wp--preset--spacing--xxx-large);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--xxx-large);padding-left:var(--wp--preset--spacing--small);min-height:100%"><span aria-hidden="true" class="wp-block-cover__background has-primary-background-color has-background-dim-80 has-background-dim"></span><img class="wp-block-cover__image-background wp-image-356" alt="Sample Image" src="https://alpha.test/wp-content/uploads/2024/05/charlie-0.0.008-pexels-suzyhazelwood-2495651-jpg.webp" data-object-fit="cover" />
+                <div class="wp-block-cover__inner-container"><!-- wp:heading {"className":"is-style-text-shadow","style":{"typography":{"fontStyle":"normal","fontWeight":"500"}},"fontSize":"max-48"} -->
                     <h2 class="wp-block-heading is-style-text-shadow has-max-48-font-size" style="font-style:normal;font-weight:500">Exciting new gallery layout!</h2>
                     <!-- /wp:heading -->
 
@@ -26,45 +26,16 @@ namespace FlexLine\flexline;
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"center","width":"65%"} -->
-        <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:65%"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small"},"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"}} -->
-            <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
-                <div class="wp-block-columns alignfull"><!-- wp:column -->
-                    <div class="wp-block-column">
-                        
-                        <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
+        <!-- wp:column {"verticalAlignment":"center","width":"","metadata":{"name":"Gallery Column"}} -->
+        <div class="wp-block-column is-vertically-aligned-center">
+            <!-- wp:group {"metadata":{"name":"Grid of Poster Galleries"},"style":{"spacing":{"blockGap":"var:preset|spacing|x-small","padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small"}}},"layout":{"type":"grid","minimumColumnWidth":"325px"}} -->
+            <div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)">
 
-                    </div>
-                    <!-- /wp:column -->
+                <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
+                <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
+                <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
+                <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
 
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                    
-                        <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
-
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-
-                <!-- wp:columns {"align":"full","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
-                <div class="wp-block-columns alignfull"><!-- wp:column -->
-                    <div class="wp-block-column">
-                        
-                        <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
-
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                       
-                        <!-- wp:pattern {"slug":"flexline/gallery-photo-poster-card"} /-->
-
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
             </div>
             <!-- /wp:group -->
         </div>
