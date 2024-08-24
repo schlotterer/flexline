42f786a87ea8801b0c8ff2f26ce5959f21d107b5
schlotterer <joelschlotterer@gmail.com>
2024-08-24 00:05:26 -0500

    baby steps

--- src/js/blocks/controls.js
+++ src/js/blocks/controls.js
@@ -1,82 +1,180 @@
-// Import necessary WordPress packages and utilities
+// Accessing WordPress packages from the 'wp' global object
 const { addFilter } = wp.hooks;
 const { createHigherOrderComponent } = wp.compose;
 const { Fragment } = wp.element;
 const { InspectorControls } = wp.blockEditor;
-const { PanelBody, SelectControl } = wp.components;
+const { PanelBody, ToggleControl, SelectControl } = wp.components;
 const { URLInput } = wp.blockEditor;
+import { getResponsiveClasses, getVisibilityControls } from "./utilities";
 
-// Import utilities for visibility controls and responsive classes
-import { getVisibilityControls, getResponsiveClasses } from './utilities';
-
+// Set up the Fields
+// Higher Order Component to add custom controls
 const withCustomControls = createHigherOrderComponent((BlockEdit) => {
 	return (props) => {
-		const { name, attributes, setAttributes } = props;
-		const blockProps = {
-			className: getResponsiveClasses(attributes),
-		};
-
-		const renderVisibilityControls = () => (
-			<PanelBody title="Flexline Visibility Options">
-				{getVisibilityControls({ attributes, setAttributes })}
-			</PanelBody>
-		);
-
-		// Define control panels for specific blocks
-		const getPanelBodyContent = () => {
-			switch (name) {
-				case 'core/image':
-					return (
-						<>
+		// Only show on specific blocks
+		if (props.name === 'core/image') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
 							<ToggleControl
 								label="Enable Lazy Load"
-								checked={!!attributes.enableLazyLoad}
+								checked={!!props.attributes.enableLazyLoad}
 								onChange={(newValue) =>
-									setAttributes({ enableLazyLoad: newValue })
+									props.setAttributes({
+										enableLazyLoad: newValue,
+									})
 								}
 							/>
 							<ToggleControl
 								label="Open a Modal"
-								checked={!!attributes.enableModal}
+								checked={!!props.attributes.enableModal}
 								onChange={(newValue) =>
-									setAttributes({ enableModal: newValue })
+									props.setAttributes({
+										enableModal: newValue,
+									})
 								}
 							/>
-							{attributes.enableModal && (
+							{props.attributes.enableModal && (
 								<URLInput
 									label="Modal Media URL"
-									value={attributes.modalMediaURL}
+									value={props.attributes.modalMediaURL}
 									onChange={(newValue) =>
-										setAttributes({
+										props.setAttributes({
 											modalMediaURL: newValue,
 										})
 									}
 								/>
 							)}
-							{renderVisibilityControls()}
-						</>
-					);
-				case 'core/cover':
-					return (
-						<>
 							<ToggleControl
-								label="Enable Lazy Load"
-								checked={!!attributes.enableLazyLoad}
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
 								onChange={(newValue) =>
-									setAttributes({ enableLazyLoad: newValue })
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
 								}
 							/>
-							{renderVisibilityControls()}
-						</>
-					);
-				case 'core/buttons':
-					return renderVisibilityControls();
-				case 'core/button':
-					return (
-						<>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		// Only show on specific blocks
+		if (props.name === 'core/cover') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
+							<ToggleControl
+								label="Enable Lazy Load"
+								checked={!!props.attributes.enableLazyLoad}
+								onChange={(newValue) =>
+									props.setAttributes({
+										enableLazyLoad: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		// Only show on specific blocks
+		if (props.name === 'core/buttons') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
+							<ToggleControl
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		if (props.name === 'core/button') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
 							<SelectControl
 								label="Icon Type"
-								value={attributes.iconType}
+								value={props.attributes.iconType}
 								options={[
 									{ label: 'None', value: 'none' },
 									{
@@ -93,82 +191,270 @@ const withCustomControls = createHigherOrderComponent((BlockEdit) => {
 										value: 'open-modal',
 									},
 									{ label: 'Link Out ↗', value: 'link-out' },
+									// Add more options as needed
 								]}
 								onChange={(newValue) =>
-									setAttributes({ iconType: newValue })
+									props.setAttributes({
+										iconType: newValue,
+									})
 								}
 							/>
 							<ToggleControl
 								label="Open Link in a Modal"
-								checked={!!attributes.enableModal}
+								checked={!!props.attributes.enableModal}
 								onChange={(newValue) =>
-									setAttributes({ enableModal: newValue })
+									props.setAttributes({
+										enableModal: newValue,
+									})
 								}
 							/>
-							{renderVisibilityControls()}
-						</>
-					);
-				case 'core/group':
-					return (
-						<>
+							<ToggleControl
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		if (props.name === 'core/gallery') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
+							<ToggleControl
+								label="Enable Poster Gallery"
+								checked={!!props.attributes.enablePosterGallery}
+								onChange={(newValue) =>
+									props.setAttributes({
+										enablePosterGallery: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		if (props.name === 'core/navigation') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
+							<ToggleControl
+								label="Enable Horizontal Scroll at Mobile"
+								checked={
+									!!props.attributes.enableHorizontalScroll
+								}
+								onChange={(newValue) =>
+									props.setAttributes({
+										enableHorizontalScroll: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		if (props.name === 'core/group') {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
 							<ToggleControl
 								label="Enable Group Link"
-								checked={!!attributes.enableGroupLink}
+								checked={!!props.attributes.enableGroupLink}
 								onChange={(newValue) =>
-									setAttributes({ enableGroupLink: newValue })
+									props.setAttributes({
+										enableGroupLink: newValue,
+									})
 								}
 							/>
-							{attributes.enableGroupLink && (
-								<>
-									<URLInput
-										label="Group Link URL"
-										value={attributes.groupLinkURL}
-										onChange={(newValue) =>
-											setAttributes({
-												groupLinkURL: newValue,
-											})
-										}
-									/>
-									<SelectControl
-										label="Link Type"
-										value={attributes.groupLinkType}
-										options={[
-											{ label: 'Normal', value: 'none' },
-											{
-												label: 'New Tab',
-												value: 'new_tab',
-											},
-											{
-												label: 'Modal Media',
-												value: 'modal_media',
-											},
-										]}
-										onChange={(newValue) =>
-											setAttributes({
-												groupLinkType: newValue,
-											})
-										}
-									/>
-								</>
+							{props.attributes.enableGroupLink && (
+								<URLInput
+									label="Group Link URL"
+									value={props.attributes.groupLinkURL}
+									onChange={(newValue) =>
+										props.setAttributes({
+											groupLinkURL: newValue,
+										})
+									}
+								/>
 							)}
-							{renderVisibilityControls()}
-						</>
-					);
-				// Add more cases for other block types as needed
-				default:
-					return renderVisibilityControls();
-			}
-		};
-
-		return (
-			<Fragment>
-				<BlockEdit {...props} {...blockProps} />
-				<InspectorControls>{getPanelBodyContent()}</InspectorControls>
-			</Fragment>
-		);
+							{props.attributes.enableGroupLink && (
+								<SelectControl
+									label="Link Type"
+									value={props.attributes.groupLinkType}
+									options={[
+										{ label: 'Normal', value: 'none' },
+										{ label: 'New Tab', value: 'new_tab' },
+										{
+											label: 'Modal Media',
+											value: 'modal_media',
+										},
+									]}
+									onChange={(newValue) =>
+										props.setAttributes({
+											groupLinkType: newValue,
+										})
+									}
+								/>
+							)}
+							<ToggleControl
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		if (
+			[
+				'core/column',
+				'core/columns',
+				'core/spacer',
+				'core/paragraph',
+				'core/heading',
+				'core/video',
+				'core/site-logo',
+				'core/post-featured-image',
+				'core/embed',
+				'core/navigation-submenu',
+				'core/navigation-link',
+				'core/html',
+				'core/social-link',
+				'core/social-links',
+			].includes(props.name)
+		) {
+			return (
+				<Fragment>
+					<BlockEdit {...props} />
+					<InspectorControls>
+						<PanelBody title="Flexline Options">
+							{props.name === 'core/columns' && (
+								<ToggleControl
+									label="Stack at Tablet"
+									checked={!!props.attributes.stackAtTablet}
+									onChange={(newValue) =>
+										props.setAttributes({
+											stackAtTablet: newValue,
+										})
+									}
+								/>
+							)}
+							<ToggleControl
+								label="Hide on Desktop"
+								checked={!!props.attributes.hideOnDesktop}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnDesktop: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Tablet"
+								checked={!!props.attributes.hideOnTablet}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnTablet: newValue,
+									})
+								}
+							/>
+							<ToggleControl
+								label="Hide on Mobile"
+								checked={!!props.attributes.hideOnMobile}
+								onChange={(newValue) =>
+									props.setAttributes({
+										hideOnMobile: newValue,
+									})
+								}
+							/>
+						</PanelBody>
+					</InspectorControls>
+				</Fragment>
+			);
+		}
+		return <BlockEdit {...props} />;
 	};
 }, 'withCustomControls');
-
 // Apply Controls filter
 addFilter(
 	'editor.BlockEdit',
