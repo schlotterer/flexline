6d7379f75f7d636319f090d9adbbf70cc71c1eb1
schlotterer <joelschlotterer@gmail.com>
2024-08-23 23:30:36 -0500

    bring in attributes and some utilities

---
+++ src/js/blocks/attributes.js
@@ -0,0 +1,80 @@
+// Set up custom attributes.
+// MediaModal - Define custom attributes
+export const customModalAttributes = {
+	enableModal: {
+		type: 'boolean',
+		default: false,
+	},
+	modalMediaURL: {
+		type: 'string',
+		default: '',
+	},
+};
+
+// Poster Gallery - Define custom attributes
+export const customGalleryAttributes = {
+	enablePosterGallery: {
+		type: 'boolean',
+		default: false,
+	},
+};
+
+// Lazy Load - Define custom attributes
+export const customLazyAttributes = {
+	enableLazyLoad: {
+		type: 'boolean',
+		default: true,
+	},
+};
+
+// Horizontal Scroll - Define custom attributes
+export const customHorizontalScrollAttributes = {
+	enableHorizontalScroll: {
+		type: 'boolean',
+		default: false,
+	},
+};
+
+// Group - Define custom attributes
+export const customGroupAttributes = {
+	enableGroupLink: {
+		type: 'boolean',
+		default: false,
+	},
+	groupLinkURL: {
+		type: 'string',
+		default: '',
+	},
+	groupLinkType: {
+		type: 'string',
+		default: 'none', // Default to 'none' indicating no link or an unselected state
+	},
+};
+
+// Visibility - Define custom attributes
+export const customVisibilityAttributes = {
+	stackAtTablet: {
+		type: 'boolean',
+		default: false,
+	},
+	hideOnDesktop: {
+		type: 'boolean',
+		default: false,
+	},
+	hideOnTablet: {
+		type: 'boolean',
+		default: false,
+	},
+	hideOnMobile: {
+		type: 'boolean',
+		default: false,
+	},
+};
+
+// Button Icons - Define custom attributes
+export const customIconAttributes = {
+	iconType: {
+		type: 'string',
+		default: 'none', // Default to 'none' indicating no icon selected
+	},
+};
