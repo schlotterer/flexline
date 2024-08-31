f58d60f17f9a73b870531ff9616b2fabdeaa0a5c
schlotterer <joelschlotterer@gmail.com>
2024-08-30 22:26:40 -0500

    add back multi row column pattern

---
+++ patterns/columns-multirow.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Title: Columns with multiple rows
+ * Slug: flexline/columns-multirow
+ * Categories: flexline-modules, flexline-sections
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"constrained"},"metadata":{"name":"Columns with multiple rows"}} -->
+<div class="wp-block-group"><!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
+    <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-outlined","layout":{"type":"constrained"},"metadata":{"name":"Info Box"}} -->
+            <div class="wp-block-group is-style-outlined"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Info Box</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">A simple box with headline and content ready for use in columns with variable styles.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-outlined","layout":{"type":"constrained"},"metadata":{"name":"Info Box"}} -->
+            <div class="wp-block-group is-style-outlined"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Info Box</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">A simple box with headline and content ready for use in columns with variable styles.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-outlined","layout":{"type":"constrained"},"metadata":{"name":"Info Box"}} -->
+            <div class="wp-block-group is-style-outlined"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Info Box</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">A simple box with headline and content ready for use in columns with variable styles.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+
+    <!-- wp:columns {"style":{"spacing":{"blockGap":{"top":"var:preset|spacing|small","left":"var:preset|spacing|small"}}}} -->
+    <div class="wp-block-columns"><!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-outlined","layout":{"type":"constrained"},"metadata":{"name":"Info Box"}} -->
+            <div class="wp-block-group is-style-outlined"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Info Box</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">A simple box with headline and content ready for use in columns with variable styles.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-outlined","layout":{"type":"constrained"},"metadata":{"name":"Info Box"}} -->
+            <div class="wp-block-group is-style-outlined"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Info Box</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">A simple box with headline and content ready for use in columns with variable styles.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column -->
+        <div class="wp-block-column"><!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"className":"is-style-outlined","layout":{"type":"constrained"},"metadata":{"name":"Info Box"}} -->
+            <div class="wp-block-group is-style-outlined"><!-- wp:heading {"level":3} -->
+                <h3 class="wp-block-heading">Info Box</h3>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"fontSize":"small"} -->
+                <p class="has-small-font-size">A simple box with headline and content ready for use in columns with variable styles.</p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
+<!-- /wp:group -->
