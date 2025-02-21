c1ff61f940721f325f7cf345d48368bd2d8e9007
schlotterer <joelschlotterer@gmail.com>
2025-02-20 20:35:08 -0600

    push iut

--- src/js/headroom.js
+++ src/js/headroom.js
@@ -51,7 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
 		}
 	}
 	const headerSiteHeader = document.querySelector('header.site-header');
-	const headroomOffset = headerSiteHeader.offsetHeight;
+	const headroomOffset = headerSiteHeader.offsetHeight + 75;
 	// OPTIONAL: define some custom options
 	const options = {
 		offset: headroomOffset,
