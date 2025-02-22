0b2893e587609a33eb66640ea403242269d0703f
schlotterer <joelschlotterer@gmail.com>
2025-02-22 10:22:29 -0600

    update header space target

--- src/js/load-early.js
+++ src/js/load-early.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	// Add padding to main content to adjust for absolute/fixed header
 	const headerSiteHeader = document.querySelector('header.site-header');
-	const mainContent = document.querySelector('main.site-content');
+	const mainContent = document.querySelector('.wp-site-blocks > main');
 	if (!headerSiteHeader.classList.contains('is-position-absolute')) {
 		mainContent.style.paddingTop = `${headerSiteHeader.offsetHeight}px`;
 	}
