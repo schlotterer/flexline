8970d06e2759563c6ca88be39a05b21de032218b
schlotterer <joelschlotterer@gmail.com>
2025-03-05 08:31:30 -0600

    New Poster Gallery Card Format adding group for better control.

--- patterns/gallery-photo-poster-card.php
+++ patterns/gallery-photo-poster-card.php
@@ -8,24 +8,47 @@
 
 namespace FlexLine\flexline;
 ?>
-<!-- wp:group {"metadata":{"name":"Poster Gallery Group - Card"},"className":"is-style-card-padded","style":{"spacing":{"blockGap":"var:preset|spacing|small"},"dimensions":{"minHeight":"100%"}},"layout":{"type":"constrained"},"groupLinkURL":"#"} -->
-<div class="wp-block-group is-style-card-padded" style="min-height:100%"><!-- wp:gallery {"columns":3,"linkTo":"media","style":{"layout":{"selfStretch":"fixed","flexSize":"250px"}},"enablePosterGallery":true} -->
-    <figure class="wp-block-gallery has-nested-images columns-3 is-cropped"><!-- wp:image {"id":893,"sizeSlug":"large","linkDestination":"media"} -->
-        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="A group of smiling seniors wearing party hats toast their champagne glasses" class="wp-image-893" /></a></figure>
+<!-- wp:group {"metadata":{"name":"Poster Gallery Group - Card"},"className":"group-link group-link-type-none is-style-default","style":{"spacing":{"blockGap":"var:preset|spacing|small"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"},"enableGroupLink":true,"groupLinkURL":"#"} -->
+<div class="wp-block-group group-link group-link-type-none is-style-default"><!-- wp:gallery {"columns":3,"linkTo":"media","className":"poster-gallery","style":{"layout":{"selfStretch":"fill","flexSize":null}},"enablePosterGallery":true} -->
+    <figure class="wp-block-gallery has-nested-images columns-3 is-cropped poster-gallery"><!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":"is-style-default"} -->
+        <figure class="wp-block-image size-large is-style-default"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
         <!-- /wp:image -->
 
-        <!-- wp:image {"id":892,"sizeSlug":"large","linkDestination":"media"} -->
-        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="Three senior woman sit at a table with cake and toast their glasses" class="wp-image-892" /></a></figure>
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
         <!-- /wp:image -->
 
-        <!-- wp:image {"id":891,"sizeSlug":"large","linkDestination":"media"} -->
-        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="A group of seniors with wine glasses toast while a smiling elderly man holds a drink bottle" class="wp-image-891" /></a></figure>
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
+        <!-- /wp:image -->
+
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
+        <!-- /wp:image -->
+
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
+        <!-- /wp:image -->
+
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
+        <!-- /wp:image -->
+
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
+        <!-- /wp:image -->
+
+        <!-- wp:image {"sizeSlug":"large","linkDestination":"media","className":""} -->
+        <figure class="wp-block-image size-large"><a href="<?php echo esc_url(feature_image_fallback()); ?>"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" /></a></figure>
         <!-- /wp:image -->
     </figure>
     <!-- /wp:gallery -->
 
-    <!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"none"}},"fontFamily":"display"} -->
-    <p class="has-text-align-center has-display-font-family" style="text-transform:none">Poster Gallery</p>
-    <!-- /wp:paragraph -->
+    <!-- wp:group {"metadata":{"name":"Title Container"},"className":"","layout":{"type":"constrained"}} -->
+    <div class="wp-block-group"><!-- wp:paragraph {"align":"center","metadata":{"name":"Poster Gallery Title"},"className":"","style":{"typography":{"textTransform":"none","lineHeight":"1.1"}},"fontFamily":"display"} -->
+        <p class="has-text-align-center has-display-font-family" style="line-height:1.1;text-transform:none">Poster Gallery Title</p>
+        <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
 </div>
 <!-- /wp:group -->
