457b7c7183096fc42e99bcbe8008d67b725bb0f0
schlotterer <joelschlotterer@gmail.com>
2025-03-06 08:55:08 -0600

    New feature - auto populate content via pattern and necessary template changes - will break old posts layouts - yolo

---
+++ patterns/template-post-starter-starter.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Title: Post Starter Starter
+ * Description: Clone this pattern and name it Post Starter to have it auto insert when a new post is created.
+ * Slug: flexline/post-starter-starter
+ * Categories: flexline-utilities
+ */
+?>
+<!-- wp:pattern {"slug":" flexline/meta-post"} /-->
+
+<!-- wp:paragraph {"className":""} -->
+<p>At [web4sl_site_name], Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Etiam porta sem malesuada magna mollis euismod.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:post-featured-image {"aspectRatio":"16/9","className":""} /-->
+
+<!-- wp:heading {"className":""} -->
+<h2 class="wp-block-heading"><strong>Sed posuere consectetur est at lobortis.</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"className":""} -->
+<p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas faucibus mollis interdum. Sed posuere consectetur est at lobortis.</p>
+<!-- /wp:paragraph -->
