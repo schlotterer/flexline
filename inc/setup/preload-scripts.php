692b20499cb9800d7df471445b9479365f127db2
Joel Schlotterer <joelschlotterer@gmail.com>
2025-06-23 17:11:36 -0500

    fix preload path for load-early.js

--- inc/setup/preload-scripts.php
+++ inc/setup/preload-scripts.php
@@ -13,6 +13,6 @@ namespace FlexLine\flexline;
  * @author Joel Schlotterer
  */
 function preload_scripts() {
-	echo '<link rel="preload" href="' . get_template_directory_uri() . '/wp-content/themes/flexline/assets/built/js/load-early.js" as="javascript"/>';
+	echo '<link rel="preload" href="' . get_template_directory_uri() . '/assets/built/js/load-early.js" as="javascript"/>';
 }
 add_action( 'wp_head', __NAMESPACE__ . '\preload_scripts', 1 );
