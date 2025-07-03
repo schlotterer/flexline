19fa1b12b132a91b8b6a8920102e785c0e2981dc
Joel Schlotterer <joelschlotterer@gmail.com>
2025-07-03 07:44:07 -0500

    clean up sticky CTA

--- patterns/cta-full-width.php
+++ patterns/cta-full-width.php
@@ -6,30 +6,25 @@
  * Categories: flexline-components, flexline-cta,
  */
 ?>
-<!-- wp:group {"metadata":{"name":"CTA - Full Width","categories":["flexline-components"],"patternName":"flexline/cta-full-width"},"align":"full","className":"is-style-shadow-dark","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"spacing":{"padding":{"top":"0","bottom":"0","left":"0","right":"0"}},"position":{"type":"sticky","top":"0px"}},"textColor":"base","gradient":"primary-primaryDark","layout":{"type":"constrained"}} -->
-<div class="wp-block-group alignfull is-style-shadow-dark has-base-color has-primary-primaryDark-gradient-background has-text-color has-background has-link-color" style="padding-top:0;padding-right:0;padding-bottom:0;padding-left:0"><!-- wp:cover {"isUserOverlayColor":true,"minHeight":50,"gradient":"primary-primaryDark","metadata":{"name":"Background Image"},"align":"full","className":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|xx-large","right":"var:preset|spacing|xx-large"}}},"layout":{"type":"constrained"}} -->
-    <div class="wp-block-cover alignfull" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--xx-large);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--xx-large);min-height:50px"><span aria-hidden="true" class="wp-block-cover__background has-background-dim-100 has-background-dim has-background-gradient has-primary-primaryDark-gradient-background"></span>
-        <div class="wp-block-cover__inner-container"><!-- wp:columns {"verticalAlignment":"center","metadata":{"name":"Content Columns"},"className":"","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}}} -->
-            <div class="wp-block-columns are-vertically-aligned-center"><!-- wp:column {"verticalAlignment":"center","className":""} -->
-                <div class="wp-block-column is-vertically-aligned-center"><!-- wp:paragraph {"align":"center","className":"","style":{"typography":{"lineHeight":"1.5"},"layout":{"columnSpan":"1"}},"fontSize":"medium","fontFamily":"display"} -->
-                    <p class="has-text-align-center has-display-font-family has-medium-font-size" style="line-height:1.5">This CTA creates a full width feature and button that can be sticky. </p>
-                    <!-- /wp:paragraph -->
-                </div>
-                <!-- /wp:column -->
-
-                <!-- wp:column {"verticalAlignment":"center","width":"250px","className":""} -->
-                <div class="wp-block-column is-vertically-aligned-center" style="flex-basis:250px"><!-- wp:buttons {"className":"","layout":{"type":"flex","justifyContent":"center"}} -->
-                    <div class="wp-block-buttons"><!-- wp:button {"className":"enable-modal  flexline-icon-open-modal","fontSize":"small","iconType":"open-modal","enableModal":true} -->
-                        <div class="wp-block-button has-custom-font-size enable-modal flexline-icon-open-modal has-small-font-size"><a class="wp-block-button__link wp-element-button">Get Started Today</a></div>
-                        <!-- /wp:button -->
-                    </div>
-                    <!-- /wp:buttons -->
-                </div>
-                <!-- /wp:column -->
-            </div>
-            <!-- /wp:columns -->
+<!-- wp:group {"metadata":{"name":"CTA - Full Width","categories":["flexline-components"],"patternName":"flexline/cta-full-width"},"align":"full","className":"is-style-shadow-dark","style":{"elements":{"link":{"color":{"text":"var:preset|color|alternate-dark"}}},"spacing":{"padding":{"top":"8px","bottom":"8px","left":"50px","right":"50px"}},"position":{"type":"sticky","top":"0px"}},"textColor":"alternate-dark","layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignfull is-style-shadow-dark has-base-color has-primary-primaryDark-gradient-background has-text-color has-background has-link-color" style="padding-top:8px;padding-right:50px;padding-bottom:8px;padding-left:50px"><!-- wp:columns {"verticalAlignment":"center","isStackedOnMobile":false,"className":"","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}}} -->
+    <div class="wp-block-columns are-vertically-aligned-center is-not-stacked-on-mobile"><!-- wp:column {"verticalAlignment":"center","width":"70%","className":"flexline-hide-on-mobile","hideOnMobile":true} -->
+        <div class="wp-block-column is-vertically-aligned-center flexline-hide-on-mobile" style="flex-basis:70%"><!-- wp:paragraph {"align":"left","className":"","style":{"typography":{"lineHeight":"1.5"},"layout":{"selfStretch":"fixed","flexSize":"720px"}},"fontSize":"large","fontFamily":"brand"} -->
+            <p class="has-text-align-left has-brand-font-family has-large-font-size" style="line-height:1.5">This CTA creates a full width feature and button that can be sticky.</p>
+            <!-- /wp:paragraph -->
         </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"center","width":"","className":""} -->
+        <div class="wp-block-column is-vertically-aligned-center"><!-- wp:buttons {"className":"","layout":{"type":"flex","justifyContent":"center"}} -->
+            <div class="wp-block-buttons"><!-- wp:button {"width":100,"className":"is-style-fill  flexline-icon-open-modal","fontSize":"small","iconType":"open-modal"} -->
+                <div class="wp-block-button has-custom-width wp-block-button__width-100 is-style-fill  flexline-icon-open-modal"><a class="wp-block-button__link has-small-font-size has-custom-font-size wp-element-button">Get Started Today</a></div>
+                <!-- /wp:button -->
+            </div>
+            <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:column -->
     </div>
-    <!-- /wp:cover -->
+    <!-- /wp:columns -->
 </div>
 <!-- /wp:group -->
