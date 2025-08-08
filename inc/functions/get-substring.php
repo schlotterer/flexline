79dac3a3113a4175a2a2a13c8a2aec1f9ce15057
Joel Schlotterer <103943917+schlotterer@users.noreply.github.com>
2025-08-07 23:24:01 -0500

    Handle missing delimiters in get_string_between

--- inc/functions/get-substring.php
+++ inc/functions/get-substring.php
@@ -20,10 +20,14 @@ namespace FlexLine\flexline;
 function get_string_between( $initial_string, $start, $end ) {
 	$initial_string = ' ' . $initial_string;
 	$ini            = strpos( $initial_string, $start );
-	if ( 0 === $ini ) {
+	if ( false === $ini ) {
 		return '';
 	}
-	$ini += strlen( $start );
-	$len  = strpos( $initial_string, $end, $ini ) - $ini;
+	$ini     += strlen( $start );
+	$end_pos  = strpos( $initial_string, $end, $ini );
+	if ( false === $end_pos || $end_pos < $ini ) {
+		return '';
+	}
+	$len = $end_pos - $ini;
 	return substr( $initial_string, $ini, $len );
 }
