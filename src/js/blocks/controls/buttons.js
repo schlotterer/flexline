d0ffc25d0c53d1943106dcd2636cc964e4416ebf
schlotterer <joelschlotterer@gmail.com>
2025-08-19 22:00:03 -0500

    2.0.1-release

--- src/js/blocks/controls/buttons.js
+++ src/js/blocks/controls/buttons.js
@@ -4,14 +4,14 @@ import { PanelBody } from '@wordpress/components';
 import { getVisibilityControls } from '../utils';
 
 export const controls = (BlockEdit, props) => (
-    <Fragment>
-        <BlockEdit {...props} />
-        <InspectorControls>
-            <PanelBody title="FlexLine Options">
-                {getVisibilityControls(props)}
-            </PanelBody>
-        </InspectorControls>
-    </Fragment>
+	<Fragment>
+		<BlockEdit {...props} />
+		<InspectorControls>
+			<PanelBody title="FlexLine Options">
+				{getVisibilityControls(props)}
+			</PanelBody>
+		</InspectorControls>
+	</Fragment>
 );
 
 export default { controls };
--- src/js/blocks/controls/navigation.js
+++ src/js/blocks/controls/navigation.js
@@ -4,29 +4,31 @@ import { PanelBody, ToggleControl } from '@wordpress/components';
 import { getVisibilityControls } from '../utils';
 
 export const controls = (BlockEdit, props) => (
-    <Fragment>
-        <BlockEdit {...props} />
-        <InspectorControls>
-            <PanelBody title="FlexLine Options">
-                <ToggleControl
-                    label="Enable Horizontal Scroll at Mobile"
-                    checked={!!props.attributes.enableHorizontalScroll}
-                    onChange={(newValue) =>
-                        props.setAttributes({ enableHorizontalScroll: newValue })
-                    }
-                />
-                {getVisibilityControls(props)}
-            </PanelBody>
-        </InspectorControls>
-    </Fragment>
+	<Fragment>
+		<BlockEdit {...props} />
+		<InspectorControls>
+			<PanelBody title="FlexLine Options">
+				<ToggleControl
+					label="Enable Horizontal Scroll at Mobile"
+					checked={!!props.attributes.enableHorizontalScroll}
+					onChange={(newValue) =>
+						props.setAttributes({
+							enableHorizontalScroll: newValue,
+						})
+					}
+				/>
+				{getVisibilityControls(props)}
+			</PanelBody>
+		</InspectorControls>
+	</Fragment>
 );
 
 export const getClasses = (attributes) => {
-    let added = '';
-    if (attributes.enableHorizontalScroll) {
-        added += ' is-style-horizontal-scroll-at-mobile';
-    }
-    return { added };
+	let added = '';
+	if (attributes.enableHorizontalScroll) {
+		added += ' is-style-horizontal-scroll-at-mobile';
+	}
+	return { added };
 };
 
 export default { controls, getClasses };
