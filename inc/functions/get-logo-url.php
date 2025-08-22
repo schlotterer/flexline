ea5b76f43a442485473f83ed88e08b488a81c7a9
Joel Schlotterer <103943917+schlotterer@users.noreply.github.com>
2025-08-22 17:13:50 -0500

    refactor: add docblock and enforce Yoda condition

--- inc/functions/get-logo-url.php
+++ inc/functions/get-logo-url.php
@@ -7,16 +7,20 @@
 
 namespace FlexLine;
 
-// Function to get the site logo URL from the Site Editor block
+/**
+ * Retrieves the site logo URL from the Site Editor block.
+ *
+ * @return string The site logo URL or an empty string if none is found.
+ */
 function get_site_logo_from_block() {
-	// Assuming the header template part is used and its slug is 'header'
+	// Assuming the header template part is used and its slug is 'header'.
 	$header_template_part = get_block_template( 'theme_slug//header', 'wp_template_part' );
 
 	if ( $header_template_part && isset( $header_template_part->content ) ) {
 		$blocks = parse_blocks( $header_template_part->content );
 
 		foreach ( $blocks as $block ) {
-			if ( $block['blockName'] === 'core/site-logo' ) {
+			if ( 'core/site-logo' === $block['blockName'] ) {
 				if ( isset( $block['attrs']['url'] ) ) {
 					return esc_url( $block['attrs']['url'] );
 				}
