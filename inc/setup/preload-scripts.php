09dc3d35393ff5d745a6ccfd491dff67c953c8c7
schlotterer <joelschlotterer@gmail.com>
2025-09-11 18:42:08 -0500

    preload it?

--- inc/setup/preload-scripts.php
+++ inc/setup/preload-scripts.php
@@ -19,5 +19,11 @@ function preload_scripts() {
 		esc_url( get_theme_file_uri( 'assets/built/js/load-early.js' ) ),
 		esc_attr( 'javascript' )
 	);
+	printf(
+		'<link rel="%1$s" href="%2$s" as="%3$s" />',
+		esc_attr( 'preload' ),
+		esc_url( get_theme_file_uri( 'assets/built/js/slider.js' ) ),
+		esc_attr( 'javascript' )
+	);
 }
 add_action( 'wp_head', __NAMESPACE__ . '\preload_scripts', 1 );
