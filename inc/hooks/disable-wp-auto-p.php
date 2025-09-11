d9b7cbc7c891a206710442c283e610b64860afa5
schlotterer <joelschlotterer@gmail.com>
2025-09-11 11:19:43 -0500

    fix this so it works

--- inc/hooks/disable-wp-auto-p.php
+++ inc/hooks/disable-wp-auto-p.php
@@ -10,13 +10,25 @@ namespace FlexLine;
 /**
  * Disables wpautop to remove empty p tags in rendered Gutenberg blocks.
  *
- * @author Joel Schlotterer
+ * Runs after the main query is available so block detection works,
+ * preventing extra <p> tags around block and shortcode output.
  */
 function disable_wpautop_for_gutenberg() {
-	// If we have blocks in place, don't add wpautop.
-	if ( has_filter( 'the_content', 'wpautop' ) && has_blocks() ) {
-		remove_filter( 'the_content', 'wpautop' );
-	}
+    // Only proceed if wpautop is currently attached to the_content.
+    if ( ! has_filter( 'the_content', 'wpautop' ) ) {
+        return;
+    }
+
+    // Determine the main queried post and check for blocks.
+    $post_id = get_queried_object_id();
+    if ( $post_id ) {
+        $post = get_post( $post_id );
+        if ( $post && has_blocks( $post->post_content ) ) {
+            remove_filter( 'the_content', 'wpautop' );
+        }
+    }
 }
 
-add_filter( 'init', __NAMESPACE__ . '\disable_wpautop_for_gutenberg', 9 );
+// Run after the main query is set so we can reliably detect blocks.
+add_action( 'wp', __NAMESPACE__ . '\\disable_wpautop_for_gutenberg', 9 );
+
