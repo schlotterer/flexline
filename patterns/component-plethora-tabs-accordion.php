a04d79aa19fce3e0d3a15d3105d3c7e3d1fcc453
schlotterer <joelschlotterer@gmail.com>
2025-11-19 11:04:42 -0600

    guard plethora patterns with php plugin check - soft deprecation

--- patterns/component-plethora-tabs-accordion.php
+++ patterns/component-plethora-tabs-accordion.php
@@ -1,14 +1,21 @@
 <?php
+if ( ! function_exists( 'is_plugin_active' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+if ( ! is_plugin_active( 'plethora-tabs-accordions/plethora-tabs-accordions.php' ) ) {
+	return;
+}
 
 /**
- * Title: Tabs
+ * Title: Tabs Accordion
  * Dependencies: Plethora Tabs + Accordions - https://plethoraplugins.com/tabs-accordions/
- * Slug: flexline/tabs
+ * Slug: flexline/tabs-accordion
  * Categories: flexline-components, flexline-misc
  */
 ?>
-<!-- wp:plethoraplugins/tabs {"tabLabels":["Tab 1","Tab 2"],"tabIds":[null,null],"className":"","metadata":{"name":"Tabs","categories":["flexline-misc"],"patternName":"flexline/tabs"}} -->
-<!-- wp:plethoraplugins/tab {"label":"Tab 1","parentLayout":"horizontal","className":""} -->
+<!-- wp:plethoraplugins/tabs {"layout":"accordion","tabLabels":["Accordion 1","Accordion 2","Accordion 3"],"tabIds":[null,null,null],"className":"","metadata":{"name":"Accordion","categories":["flexline-misc"],"patternName":"flexline/tabs-accordion"}} -->
+<!-- wp:plethoraplugins/tab {"label":"Accordion 1","parentLayout":"accordion","className":""} -->
 <!-- wp:heading {"className":""} -->
 <h2 class="wp-block-heading">Sample Section</h2>
 <!-- /wp:heading -->
@@ -18,9 +25,19 @@
 <!-- /wp:paragraph -->
 <!-- /wp:plethoraplugins/tab -->
 
-<!-- wp:plethoraplugins/tab {"label":"Tab 2","parentLayout":"horizontal","className":""} -->
+<!-- wp:plethoraplugins/tab {"label":"Accordion 2","parentLayout":"accordion","className":""} -->
 <!-- wp:heading {"className":""} -->
 <h2 class="wp-block-heading">Sample Section 2</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"placeholder":"Tab content...","className":""} -->
+<p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec sed odio dui. Maecenas faucibus mollis interdum. Aenean lacinia bibendum nulla sed consectetur. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Maecenas sed diam eget risus varius blandit sit amet non magna.<br>Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Cras mattis consectetur purus sit amet fermentum. Donec id elit non mi porta gravida at eget metus. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:plethoraplugins/tab -->
+
+<!-- wp:plethoraplugins/tab {"label":"Accordion 3","parentLayout":"accordion","className":""} -->
+<!-- wp:heading {"className":""} -->
+<h2 class="wp-block-heading">Sample Section 3</h2>
 <!-- /wp:heading -->
 
 <!-- wp:paragraph {"placeholder":"Tab content...","className":""} -->
