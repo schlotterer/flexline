607b1cebb13f5045285f090809bf64be677c3ea5
schlotterer <joelschlotterer@gmail.com>
2025-11-28 12:10:19 -0600

    clean up accordion pattern

--- patterns/component-accordion.php
+++ patterns/component-accordion.php
@@ -4,13 +4,13 @@
  * Slug: flexline/accordion
  * Categories: flexline-components, flexline-misc
  */
+
 namespace FlexLine;
 ?>
-
-<!-- wp:accordion {"autoclose":true,"headingLevel":2,"className":"is-style-default"} -->
+<!-- wp:accordion {"autoclose":true,"headingLevel":2,"className":"is-style-default","style":{"spacing":{"blockGap":"var:preset|spacing|small"}}} -->
 <div role="group" class="wp-block-accordion is-style-default"><!-- wp:accordion-item {"metadata":{"name":"Accordion Item - Card"},"className":"is-style-card","style":{"spacing":{"blockGap":"0"}}} -->
-<div class="wp-block-accordion-item is-style-card"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"textDecoration":"none"}},"backgroundColor":"secondary","textColor":"base","fontSize":"medium","fontFamily":"display"} -->
-<h2 class="wp-block-accordion-heading is-style-default has-base-color has-secondary-background-color has-text-color has-background has-link-color has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Card Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
+<div class="wp-block-accordion-item is-style-card"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"textDecoration":"none"}},"backgroundColor":"secondary","textColor":"base","fontSize":"medium","fontFamily":"display"} -->
+<h2 class="wp-block-accordion-heading is-style-default has-base-color has-secondary-background-color has-text-color has-background has-link-color has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Card Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
 <!-- /wp:accordion-heading -->
 
 <!-- wp:accordion-panel {"className":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}},"backgroundColor":"base"} -->
@@ -23,7 +23,7 @@ namespace FlexLine;
 
 <!-- wp:column {"width":"33.33%","className":""} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":""} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" class=""/></figure>
+<figure class="wp-block-image size-full"><img src="https://flexlinetheme.com/wp-content/uploads/2024/04/fallback-large.webp" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
@@ -31,8 +31,8 @@ namespace FlexLine;
 <!-- /wp:accordion-item -->
 
 <!-- wp:accordion-item {"metadata":{"name":"Accordion Item - Card Mono"},"className":"is-style-card","style":{"spacing":{"blockGap":"0"}},"backgroundColor":"secondary"} -->
-<div class="wp-block-accordion-item is-style-card has-secondary-background-color has-background"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"textDecoration":"none"}},"textColor":"base","fontSize":"medium","fontFamily":"display"} -->
-<h2 class="wp-block-accordion-heading is-style-default has-base-color has-text-color has-link-color has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Mono Card Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
+<div class="wp-block-accordion-item is-style-card has-secondary-background-color has-background"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"textDecoration":"none"}},"textColor":"base","fontSize":"medium","fontFamily":"display"} -->
+<h2 class="wp-block-accordion-heading is-style-default has-base-color has-text-color has-link-color has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Mono Card Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
 <!-- /wp:accordion-heading -->
 
 <!-- wp:accordion-panel {"className":"","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}}},"textColor":"base"} -->
@@ -43,13 +43,13 @@ namespace FlexLine;
 <!-- /wp:accordion-item -->
 
 <!-- wp:accordion-item {"metadata":{"name":"Accordion Item - Outline"},"className":"is-style-outlined","style":{"spacing":{"blockGap":"0"}}} -->
-<div class="wp-block-accordion-item is-style-outlined"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}},"typography":{"textDecoration":"none"}},"fontSize":"medium","fontFamily":"display"} -->
-<h2 class="wp-block-accordion-heading is-style-default has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Outline Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
+<div class="wp-block-accordion-item is-style-outlined"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}},"typography":{"textDecoration":"none"}},"fontSize":"medium","fontFamily":"display"} -->
+<h2 class="wp-block-accordion-heading is-style-default has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:0;padding-right:var(--wp--preset--spacing--small);padding-bottom:0;padding-left:var(--wp--preset--spacing--small)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Outline Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
 <!-- /wp:accordion-heading -->
 
 <!-- wp:accordion-panel {"className":"","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}}}} -->
 <div role="region" class="wp-block-accordion-panel" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:image {"aspectRatio":"16/9","scale":"cover","sizeSlug":"full","linkDestination":"none","className":"is-style-card"} -->
-<figure class="wp-block-image size-full is-style-card"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" class="" style="aspect-ratio:16/9;object-fit:cover"/></figure>
+<figure class="wp-block-image size-full is-style-card"><img src="https://flexlinetheme.com/wp-content/uploads/2024/04/fallback-large.webp" alt="" style="aspect-ratio:16/9;object-fit:cover"/></figure>
 <!-- /wp:image -->
 
 <!-- wp:paragraph {"className":""} -->
@@ -59,8 +59,8 @@ namespace FlexLine;
 <!-- /wp:accordion-item -->
 
 <!-- wp:accordion-item {"metadata":{"name":"Accordion Item - Glass Panel"},"className":"is-style-glass-card-no-padding","style":{"spacing":{"blockGap":"0"}}} -->
-<div class="wp-block-accordion-item is-style-glass-card-no-padding"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"textDecoration":"none"}},"backgroundColor":"secondary","textColor":"base","fontSize":"medium","fontFamily":"display"} -->
-<h2 class="wp-block-accordion-heading is-style-default has-base-color has-secondary-background-color has-text-color has-background has-link-color has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--medium);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--medium);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Card with Glass Panel</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
+<div class="wp-block-accordion-item is-style-glass-card-no-padding"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"textDecoration":"none"}},"backgroundColor":"secondary","textColor":"base","fontSize":"medium","fontFamily":"display"} -->
+<h2 class="wp-block-accordion-heading is-style-default has-base-color has-secondary-background-color has-text-color has-background has-link-color has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Card with Glass Panel</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
 <!-- /wp:accordion-heading -->
 
 <!-- wp:accordion-panel {"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|medium","bottom":"var:preset|spacing|medium","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
@@ -73,20 +73,20 @@ namespace FlexLine;
 
 <!-- wp:column {"width":"33.33%","className":""} -->
 <div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:image {"sizeSlug":"full","linkDestination":"none","className":""} -->
-<figure class="wp-block-image size-full"><img src="<?php echo esc_url(feature_image_fallback()); ?>" alt="" class=""/></figure>
+<figure class="wp-block-image size-full"><img src="https://flexlinetheme.com/wp-content/uploads/2024/04/fallback-large.webp" alt=""/></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
 <!-- /wp:accordion-panel --></div>
 <!-- /wp:accordion-item -->
 
-<!-- wp:accordion-item {"metadata":{"name":"Accordion Item - Glass Card"},"className":"is-style-glass-card","style":{"spacing":{"blockGap":"0"},"color":{"background":"#ffffff91"}}} -->
-<div class="wp-block-accordion-item is-style-glass-card has-background" style="background-color:#ffffff91"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}},"typography":{"textDecoration":"none"}},"fontSize":"medium","fontFamily":"display"} -->
-<h2 class="wp-block-accordion-heading is-style-default has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Glass Card Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
+<!-- wp:accordion-item {"metadata":{"name":"Accordion Item - Glass Card"},"className":"is-style-glass-card-no-padding","style":{"spacing":{"blockGap":"0"},"color":{"background":"#ffffff91"}}} -->
+<div class="wp-block-accordion-item is-style-glass-card-no-padding has-background" style="background-color:#ffffff91"><!-- wp:accordion-heading {"level":2,"className":"is-style-default","style":{"spacing":{"padding":{"top":"var:preset|spacing|small","bottom":"var:preset|spacing|small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}},"typography":{"textDecoration":"none"}},"fontSize":"medium","fontFamily":"display"} -->
+<h2 class="wp-block-accordion-heading is-style-default has-display-font-family has-medium-font-size"><button class="wp-block-accordion-heading__toggle" style="padding-top:var(--wp--preset--spacing--small);padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><span class="wp-block-accordion-heading__toggle-title" style="text-decoration:none">Accordion - Glass Card Style</span><span class="wp-block-accordion-heading__toggle-icon" aria-hidden="true">+</span></button></h2>
 <!-- /wp:accordion-heading -->
 
-<!-- wp:accordion-panel {"className":"","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|small","left":"var:preset|spacing|small","right":"var:preset|spacing|small"}}}} -->
-<div role="region" class="wp-block-accordion-panel" style="padding-top:0;padding-right:var(--wp--preset--spacing--small);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--small)"><!-- wp:paragraph {"className":""} -->
+<!-- wp:accordion-panel {"isSelected":true,"className":"","style":{"spacing":{"padding":{"top":"0","bottom":"var:preset|spacing|small","left":"var:preset|spacing|medium","right":"var:preset|spacing|medium"}}}} -->
+<div role="region" class="wp-block-accordion-panel" style="padding-top:0;padding-right:var(--wp--preset--spacing--medium);padding-bottom:var(--wp--preset--spacing--small);padding-left:var(--wp--preset--spacing--medium)"><!-- wp:paragraph {"className":""} -->
 <p>Accordion content can use any blocks. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Cras mattis consectetur purus sit amet fermentum.<br>Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:accordion-panel --></div>
