c1401d457047f0b9b2ae3ccfc2aeab668f16e3d8
schlotterer <joelschlotterer@gmail.com>
2026-05-22 17:07:08 -0500

    on mount if wp7 update to new version

--- src/js/blocks/controls/gallery.js
+++ src/js/blocks/controls/gallery.js
@@ -1,4 +1,4 @@
-import { Fragment } from '@wordpress/element';
+import { Fragment, useEffect } from '@wordpress/element';
 import { InspectorControls } from '@wordpress/block-editor';
 import { PanelBody, ToggleControl } from '@wordpress/components';
 import { useDispatch, useSelect } from '@wordpress/data';
@@ -40,6 +40,138 @@ const getImageLightboxAttribute = (imageAttributes, lightboxSetting) => {
 	};
 };
 
+const getGalleryImageBlocks = (galleryBlock) =>
+	galleryBlock?.innerBlocks?.filter((block) => block.name === 'core/image') ||
+	[];
+
+const getDesiredImageAttributes = (imageAttributes, lightboxSetting) => ({
+	href: undefined,
+	linkDestination: IMAGE_LINK_DESTINATION_NONE,
+	lightbox: getImageLightboxAttribute(imageAttributes, lightboxSetting),
+});
+
+const hasLightboxDrift = (currentLightbox, desiredLightbox) => {
+	if (!desiredLightbox) {
+		return currentLightbox !== undefined;
+	}
+
+	if (!currentLightbox || typeof currentLightbox !== 'object') {
+		return true;
+	}
+
+	return currentLightbox.enabled !== desiredLightbox.enabled;
+};
+
+const getPosterGallerySyncPayload = (imageBlocks, lightboxSetting) => {
+	const changedAttributes = {};
+	const targetClientIds = [];
+
+	imageBlocks.forEach((block) => {
+		const desired = getDesiredImageAttributes(
+			block.attributes,
+			lightboxSetting
+		);
+		const hasDrift =
+			block.attributes.href !== desired.href ||
+			block.attributes.linkDestination !== desired.linkDestination ||
+			hasLightboxDrift(block.attributes.lightbox, desired.lightbox);
+
+		if (!hasDrift) {
+			return;
+		}
+
+		targetClientIds.push(block.clientId);
+		changedAttributes[block.clientId] = desired;
+	});
+
+	return { targetClientIds, changedAttributes };
+};
+
+const syncPosterGalleryImages = ({
+	clientId,
+	getBlock,
+	getSettings,
+	updateBlockAttributes,
+}) => {
+	const galleryBlock = getBlock(clientId);
+	const imageBlocks = getGalleryImageBlocks(galleryBlock);
+
+	if (!imageBlocks.length) {
+		return;
+	}
+
+	const lightboxSetting = getEditorLightboxSetting(getSettings?.());
+	const { targetClientIds, changedAttributes } = getPosterGallerySyncPayload(
+		imageBlocks,
+		lightboxSetting
+	);
+
+	if (!targetClientIds.length) {
+		return;
+	}
+
+	updateBlockAttributes(targetClientIds, changedAttributes, {
+		uniqueByBlock: true,
+	});
+};
+
+const useHooks = (props) => {
+	const { getBlock, getSettings } = useSelect(
+		(select) => select('core/block-editor'),
+		[]
+	);
+	const { updateBlockAttributes } = useDispatch('core/block-editor');
+	const imageSyncFingerprint = useSelect(
+		(select) => {
+			const blockEditor = select('core/block-editor');
+			const galleryBlock = blockEditor.getBlock(props.clientId);
+			const imageBlocks = getGalleryImageBlocks(galleryBlock);
+
+			return imageBlocks
+				.map(
+					(block) =>
+						`${block.clientId}|${block.attributes.linkDestination ?? ''}|${
+							block.attributes.href ?? ''
+						}|${
+							block.attributes.lightbox &&
+							typeof block.attributes.lightbox === 'object'
+								? block.attributes.lightbox.enabled
+								: ''
+						}`
+				)
+				.join('||');
+		},
+		[props.clientId]
+	);
+
+	useEffect(() => {
+		if (
+			!(
+				props.attributes.enablePosterGallery &&
+				shouldUseCoreGalleryLightbox()
+			)
+		) {
+			return;
+		}
+
+		syncPosterGalleryImages({
+			clientId: props.clientId,
+			getBlock,
+			getSettings,
+			updateBlockAttributes,
+		});
+	}, [
+		props.clientId,
+		props.attributes.enablePosterGallery,
+		props.attributes.linkTo,
+		props.attributes.className,
+		imageSyncFingerprint,
+		getBlock,
+		getSettings,
+		updateBlockAttributes,
+	]);
+};
+
 const Controls = ({ BlockEdit, ...props }) => {
 	const { getBlock, getSettings } = useSelect(
 		(select) => select('core/block-editor'),
@@ -54,34 +186,11 @@ const Controls = ({ BlockEdit, ...props }) => {
 			return;
 		}
 
-		const galleryBlock = getBlock(props.clientId);
-		const imageBlocks =
-			galleryBlock?.innerBlocks?.filter(
-				(block) => block.name === 'core/image'
-			) || [];
-
-		if (!imageBlocks.length) {
-			return;
-		}
-
-		const lightboxSetting = getEditorLightboxSetting(getSettings?.());
-		const changedAttributes = {};
-		const targetClientIds = [];
-
-		imageBlocks.forEach((block) => {
-			targetClientIds.push(block.clientId);
-			changedAttributes[block.clientId] = {
-				href: undefined,
-				linkDestination: IMAGE_LINK_DESTINATION_NONE,
-				lightbox: getImageLightboxAttribute(
-					block.attributes,
-					lightboxSetting
-				),
-			};
-		});
-
-		updateBlockAttributes(targetClientIds, changedAttributes, {
-			uniqueByBlock: true,
+		syncPosterGalleryImages({
+			clientId: props.clientId,
+			getBlock,
+			getSettings,
+			updateBlockAttributes,
 		});
 	};
 
@@ -113,4 +222,4 @@ export const getClasses = (attributes) => {
 	return { added };
 };
 
-export default { controls, getClasses };
+export default { controls, getClasses, useHooks };
